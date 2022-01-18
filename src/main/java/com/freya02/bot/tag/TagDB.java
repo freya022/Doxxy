7463db9f7791f740e06930ea3ca229159d13cb8b
freya022 <41875020+freya022@users.noreply.github.com>
2022-01-18 12:19:36 +0100

    WIP TagDB refactoring

--- src/main/java/com/freya02/bot/tag/TagDB.java
+++ src/main/java/com/freya02/bot/tag/TagDB.java
@@ -2,6 +2,9 @@ package com.freya02.bot.tag;
 
 import com.freya02.bot.Database;
 import com.freya02.bot.utils.Utils;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,14 +24,29 @@ public class TagDB {
 
 		final String setupSql = Utils.readResource("TagDB.sql");
 		try (Connection connection = database.getConnection()) {
-			connection.prepareStatement(setupSql).execute();
+			//TODO need to fill restrictions
+			// see how to make alter tables so restrictions are kept up-to-date
+			//   internal checks with methods checkName, checkDescription, checkText
+			final PreparedStatement statement = connection.prepareStatement(setupSql);
+
+			//Setting column restrictions to JDA constants
+			statement.setObject(1, OptionData.MAX_CHOICE_NAME_LENGTH);
+			statement.setObject(2, MessageEmbed.VALUE_MAX_LENGTH);
+			statement.setObject(3, Message.MAX_CONTENT_LENGTH);
+
+			statement.execute();
 		}
 	}
 
 	public void create(long guildId, long ownerId, String name, String description, String text) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("insert into Tag (guildid, ownerid, name, description, text) values (?, ?, ?, ?, ?)");
+			final PreparedStatement statement = connection.prepareStatement(
+					//TODO use text blocks
+					"insert into Tag (guildid, ownerid, name, description, text) " +
+							"values (?, ?, ?, ?, ?)"
+			);
 
+			//TODO abstraction
 			statement.setLong(1, guildId);
 			statement.setLong(2, ownerId);
 			statement.setString(3, name);
@@ -41,7 +59,10 @@ public class TagDB {
 
 	public void edit(long guildId, long ownerId, String name, String description, String text) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("update Tag set description = ?, text = ? where guildid = ? and ownerid = ? and name = ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"update Tag set description = ?, text = ? " +
+							"where guildid = ? and ownerid = ? and name = ?"
+			);
 
 			statement.setString(1, description);
 			statement.setString(2, text);
@@ -55,7 +76,10 @@ public class TagDB {
 
 	public void delete(long guildId, long ownerId, String name) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("delete from Tag where guildid = ? and ownerid = ? and name = ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"delete from Tag " +
+							"where guildid = ? and ownerid = ? and name = ?"
+			);
 
 			statement.setLong(1, guildId);
 			statement.setLong(2, ownerId);
@@ -68,7 +92,10 @@ public class TagDB {
 	@Nullable
 	public Tag get(long guildId, String name) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("select * from Tag where guildid = ? and name = ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"select * from Tag " +
+							"where guildid = ? and name = ?"
+			);
 
 			statement.setLong(1, guildId);
 			statement.setString(2, name);
@@ -83,7 +110,10 @@ public class TagDB {
 
 	public void incrementTag(long guildId, String name) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("update Tag set uses = uses + 1 where guildid = ? and name = ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"update Tag set uses = uses + 1 " +
+							"where guildid = ? and name = ?"
+			);
 
 			statement.setLong(1, guildId);
 			statement.setString(2, name);
@@ -109,7 +139,9 @@ public class TagDB {
 
 	public int getTotalTags(long guildId) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("select count(*) from Tag where guildid = ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"select count(*) from Tag where guildid = ?"
+			);
 
 			statement.setLong(1, guildId);
 
@@ -122,7 +154,13 @@ public class TagDB {
 
 	public List<Tag> getTagRange(long guildId, TagCriteria criteria, int offset, int amount) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("select * from Tag where guildid = ? order by " + criteria.getKey() + " offset ? limit ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"select * from Tag " +
+							"where guildid = ? " +
+							"order by " + criteria.getKey() + " " +
+							"offset ? " +
+							"limit ?"
+			);
 
 			statement.setLong(1, guildId);
 			statement.setInt(2, offset);
@@ -141,7 +179,11 @@ public class TagDB {
 
 	public List<ShortTag> getShortTagsSorted(long guildId, TagCriteria criteria) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("select name, description from Tag where guildid = ? order by " + criteria.getKey());
+			final PreparedStatement statement = connection.prepareStatement(
+					"select name, description from Tag " +
+							"where guildid = ? " +
+							"order by " + criteria.getKey()
+			);
 			statement.setLong(1, guildId);
 
 			return readShortTags(statement);
@@ -150,7 +192,11 @@ public class TagDB {
 
 	public List<ShortTag> getShortTagsSorted(long guildId, long ownerId, TagCriteria criteria) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("select name, description from Tag where guildid = ? and ownerid = ? order by " + criteria.getKey());
+			final PreparedStatement statement = connection.prepareStatement(
+					"select name, description from Tag " +
+							"where guildid = ? and ownerid = ? " +
+							"order by " + criteria.getKey()
+			);
 			statement.setLong(1, guildId);
 			statement.setLong(2, ownerId);
 
@@ -160,7 +206,12 @@ public class TagDB {
 
 	public long getRank(long guildId, String name) throws SQLException {
 		try (Connection connection = database.getConnection()) {
-			final PreparedStatement statement = connection.prepareStatement("select rank from (select name, dense_rank() over (order by uses desc) as rank from Tag where guildid = ?) as ranks where name = ?");
+			final PreparedStatement statement = connection.prepareStatement(
+					"select rank from " +
+							"(select name, dense_rank() over (order by uses desc) as rank from Tag " +
+							"where guildid = ?) as ranks " +
+							"where name = ?"
+			);
 			statement.setLong(1, guildId);
 			statement.setString(2, name);
 
