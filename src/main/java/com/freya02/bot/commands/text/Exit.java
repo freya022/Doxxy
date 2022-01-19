57ac43e1f38edf12730b411ad2a1e465b5cc2b0a
freya02 <41875020+freya022@users.noreply.github.com>
2022-01-19 19:14:53 +0100

    React success on exit, shutdown JDA

--- src/main/java/com/freya02/bot/commands/text/Exit.java
+++ src/main/java/com/freya02/bot/commands/text/Exit.java
@@ -5,6 +5,7 @@ import com.freya02.botcommands.api.annotations.RequireOwner;
 import com.freya02.botcommands.api.prefixed.BaseCommandEvent;
 import com.freya02.botcommands.api.prefixed.TextCommand;
 import com.freya02.botcommands.api.prefixed.annotations.JDATextCommand;
+import net.dv8tion.jda.api.JDA;
 import org.slf4j.Logger;
 
 public class Exit extends TextCommand {
@@ -12,8 +13,18 @@ public class Exit extends TextCommand {
 
 	@RequireOwner
 	@JDATextCommand(name = "exit")
-	public void say(BaseCommandEvent event) {
+	public void say(BaseCommandEvent event) throws InterruptedException {
 		LOGGER.warn("Shutdown initiated by {} ({})", event.getAuthor().getAsTag(), event.getAuthor().getId());
+
+		event.reactSuccess().mapToResult().complete();
+
+		event.getJDA().shutdown();
+
+		while (event.getJDA().getStatus() != JDA.Status.SHUTDOWN) {
+			Thread.sleep(15);
+
+			Thread.onSpinWait();
+		}
 
 		System.exit(0);
 	}
