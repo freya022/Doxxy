def2c812a0e28bd168fc5fce3dada9e1635408eb
freya022 <41875020+freya022@users.noreply.github.com>
2022-01-24 11:08:19 +0100

    Use # instead of . for method references

--- src/main/java/com/freya02/docs/SeeAlso.java
+++ src/main/java/com/freya02/docs/SeeAlso.java
@@ -13,7 +13,18 @@ public class SeeAlso {
 
 	public SeeAlso(@NotNull DocDetail docDetail) {
 		for (Element seeAlsoClassElement : docDetail.getHtmlElements().get(0).getTargetElement().select("dd > ul > li > a")) {
-			references.add(new SeeAlsoReference(seeAlsoClassElement.text(), seeAlsoClassElement.absUrl("href")));
+			final String text = seeAlsoClassElement.text();
+
+			final StringBuilder textBuilder = new StringBuilder(text);
+			final int index = text.lastIndexOf('.');
+
+			if (index > -1) {
+				textBuilder.replace(index, index + 1, "#");
+			}
+
+			//TODO maybe add another field to put resolved full methods signatures
+			// Will be useful for the see also selection menu
+			references.add(new SeeAlsoReference(textBuilder.toString(), seeAlsoClassElement.absUrl("href")));
 		}
 	}
 
