608a08057211c95ad0b4555ad7054cceeebc50f9
freya022 <41875020+freya022@users.noreply.github.com>
2022-01-25 10:44:34 +0100

    Put default value for `/maven`

--- src/main/java/com/freya02/bot/commands/slash/versioning/SlashMaven.java
+++ src/main/java/com/freya02/bot/commands/slash/versioning/SlashMaven.java
@@ -4,6 +4,7 @@ import com.freya02.bot.commands.slash.DeleteButtonListener;
 import com.freya02.bot.versioning.ArtifactInfo;
 import com.freya02.bot.versioning.LibraryType;
 import com.freya02.bot.versioning.Versions;
+import com.freya02.botcommands.api.annotations.Optional;
 import com.freya02.botcommands.api.application.ApplicationCommand;
 import com.freya02.botcommands.api.application.annotations.AppOption;
 import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
@@ -74,11 +75,11 @@ public class SlashMaven extends ApplicationCommand {
 			description = "Shows the maven dependencies for a library"
 	)
 	public void onSlashMaven(GuildSlashEvent event,
-	                         @AppOption(description = "Type of library") LibraryType libraryType) {
+	                         @Optional @AppOption(description = "Type of library") LibraryType libraryType) {
 		final EmbedBuilder builder = new EmbedBuilder();
 
 		final String xml;
-		if (libraryType == LibraryType.BOT_COMMANDS) {
+		if (libraryType == null || libraryType == LibraryType.BOT_COMMANDS) { //Default
 			final ArtifactInfo latestBotCommands = versions.getLatestBotCommandsVersion();
 			final ArtifactInfo jdaVersionFromBotCommands = versions.getJdaVersionFromBotCommands();
 
