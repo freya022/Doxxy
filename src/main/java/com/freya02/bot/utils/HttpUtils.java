581021983cd732603d09a000d3bbf18a37a3ab41
freya02 <41875020+freya022@users.noreply.github.com>
2022-02-07 17:44:49 +0100

    Remove unused methods

--- src/main/java/com/freya02/bot/utils/HttpUtils.java
+++ src/main/java/com/freya02/bot/utils/HttpUtils.java
@@ -10,7 +10,6 @@ import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
@@ -60,14 +59,6 @@ public class HttpUtils {
 		return parseDocument(downloadBody(url), url);
 	}
 
-	public static void sendRequest(String url) throws IOException {
-		CLIENT.newCall(new Request.Builder()
-						.url(url)
-						.build())
-				.execute()
-				.close();
-	}
-
 	@NotNull
 	public static String downloadBody(String url) throws IOException {
 		try (Response response = CLIENT.newCall(new Request.Builder()
@@ -79,17 +70,6 @@ public class HttpUtils {
 
 			return body.string();
 		}
-	}
-
-	public static Path getCachePathForUrl(String url) {
-		final HttpUrl httpUrl = HttpUrl.get(url);
-
-		Path path = Main.CACHE_PATH.resolve(httpUrl.host());
-		for (String segment : httpUrl.pathSegments()) {
-			path = path.resolve(segment);
-		}
-
-		return path;
 	}
 
 	/**
@@ -126,80 +106,4 @@ public class HttpUtils {
 
 		return url;
 	}
-
-	//In case OkHttp caching isn't accurate, use code below and transform it to use ETag(s)
-//	private static Path changeExtension(Path path, String newExtension) {
-//		final String fileName = path.getFileName().toString();
-//
-//		return path.resolveSibling(fileName.substring(0, fileName.lastIndexOf('.') + 1) + newExtension);
-//	}
-
-//	private static String downloadBody(@NotNull String url) throws IOException {
-//		final HttpUrl httpUrl = HttpUrl.get(url);
-//		Path filePath = CACHE_PATH.resolve(httpUrl.host());
-//		for (String segment : httpUrl.pathSegments()) {
-//			filePath = filePath.resolve(segment);
-//		}
-//
-//		final Path lastModifiedPath = changeExtension(filePath, "lastmodified");
-//		if (Files.exists(lastModifiedPath)) {
-//			//Try to ask the server, and check the 304 status if the file has NOT changed
-//
-//			try (Response response = CLIENT.newCall(new Request.Builder()
-//					.url(url)
-//					.head()
-//					.header("If-Modified-Since", Files.readString(lastModifiedPath))
-//					.build()
-//			).execute()) {
-//				if (response.code() == 200) { //File modified
-//					return updateDownloadedResource(url, filePath, lastModifiedPath, response);
-//				} else if (response.code() == 304) { //File not modified, read from disk
-//					return Files.readString(filePath);
-//				} else {
-//					throw new IllegalArgumentException("Unexpected HTTP code on if-modified call: " + response.code());
-//				}
-//			}
-//		} else {
-//			try (Response response = CLIENT.newCall(new Request.Builder()
-//					.url(url)
-//					.build()
-//			).execute()) {
-//				if (response.isSuccessful()) {
-//					return updateDownloadedResource(url, filePath, lastModifiedPath, response);
-//				} else {
-//					throw new IllegalArgumentException("Unexpected HTTP code on initial get: " + response.code());
-//				}
-//			}
-//		}
-//	}
-//
-//	@NotNull
-//	private static String updateDownloadedResource(@NotNull String url, Path filePath, Path lastModifiedPath, Response response) throws IOException {
-//		final String body = downloadResponse(filePath, response);
-//
-//		final String lastModifiedHeader = response.header("Last-Modified");
-//		if (lastModifiedHeader == null)
-//			throw new IllegalArgumentException("Got no Last-Modified header from If-Modified-Since response on url " + url);
-//
-//		Files.writeString(lastModifiedPath, lastModifiedHeader, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-//
-//		return body;
-//	}
-//
-//	@NotNull
-//	private static String downloadResponse(Path filePath, Response response) throws IOException {
-//		String input;
-//		final Path tempFile = Files.createTempFile("doc", ".html");
-//
-//		final ResponseBody body = response.body();
-//		if (body == null) throw new IllegalArgumentException("Got no body for url: " + response.request().url());
-//
-//		input = body.string();
-//		Files.writeString(tempFile, input, StandardOpenOption.WRITE);
-//
-//		Files.createDirectories(filePath.getParent());
-//		Files.move(tempFile, filePath, StandardCopyOption.ATOMIC_MOVE);
-//
-//		return input;
-//	}
 }
