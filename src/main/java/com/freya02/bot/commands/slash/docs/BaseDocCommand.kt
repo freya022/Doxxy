2e91721d6dcabde81024c48312e14b057ff3841c
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-08 16:03:28 +0200

    Convert BaseDocCommand

--- src/main/java/com/freya02/bot/commands/slash/docs/BaseDocCommand.kt
+++ src/main/java/com/freya02/bot/commands/slash/docs/BaseDocCommand.kt
@@ -1,40 +1,30 @@
-package com.freya02.bot.commands.slash.docs;
+package com.freya02.bot.commands.slash.docs
 
-import com.freya02.bot.docs.DocIndexMap;
-import com.freya02.botcommands.api.BContext;
-import com.freya02.botcommands.api.application.ApplicationCommand;
-import com.freya02.botcommands.api.application.CommandPath;
-import com.freya02.botcommands.api.application.slash.DefaultValueSupplier;
-import com.freya02.docs.DocSourceType;
-import net.dv8tion.jda.api.entities.Guild;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.freya02.bot.docs.DocIndexMap
+import com.freya02.botcommands.api.BContext
+import com.freya02.botcommands.api.application.ApplicationCommand
+import com.freya02.botcommands.api.application.CommandPath
+import com.freya02.botcommands.api.application.slash.DefaultValueSupplier
+import com.freya02.docs.DocSourceType
+import net.dv8tion.jda.api.entities.Guild
 
-import java.io.IOException;
+abstract class BaseDocCommand : ApplicationCommand() {
+    protected val docIndexMap: DocIndexMap = DocIndexMap.getInstance()
 
-public abstract class BaseDocCommand extends ApplicationCommand {
-	protected final DocIndexMap docIndexMap;
+    override fun getDefaultValueSupplier(
+        context: BContext, guild: Guild,
+        commandId: String?, commandPath: CommandPath,
+        optionName: String, parameterType: Class<*>
+    ): DefaultValueSupplier? {
+        if (optionName == "source_type") {
+            //use subcommand as a default value
+            when (commandPath.subname) {
+                "botcommands" -> return DefaultValueSupplier { DocSourceType.BOT_COMMANDS }
+                "jda" -> return DefaultValueSupplier { DocSourceType.JDA }
+                "java" -> return DefaultValueSupplier { DocSourceType.JAVA }
+            }
+        }
 
-	public BaseDocCommand() throws IOException {
-		docIndexMap = DocIndexMap.getInstance();
-	}
-
-	@Override
-	@Nullable
-	public DefaultValueSupplier getDefaultValueSupplier(@NotNull BContext context, @NotNull Guild guild,
-	                                                    @Nullable String commandId, @NotNull CommandPath commandPath,
-	                                                    @NotNull String optionName, @NotNull Class<?> parameterType) {
-		if (optionName.equals("source_type")) {
-			//use subcommand as a default value
-			if ("botcommands".equals(commandPath.getSubname())) {
-				return e -> DocSourceType.BOT_COMMANDS;
-			} else if ("jda".equals(commandPath.getSubname())) {
-				return e -> DocSourceType.JDA;
-			} else if ("java".equals(commandPath.getSubname())) {
-				return e -> DocSourceType.JAVA;
-			}
-		}
-
-		return super.getDefaultValueSupplier(context, guild, commandId, commandPath, optionName, parameterType);
-	}
+        return super.getDefaultValueSupplier(context, guild, commandId, commandPath, optionName, parameterType)
+    }
 }
