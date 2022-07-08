934a037408cca16a23e22958e45c3b7a0f4176af
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-08 16:00:04 +0200

    Convert ClassCommand

--- src/main/java/com/freya02/bot/commands/slash/docs/ClassCommand.kt
+++ src/main/java/com/freya02/bot/commands/slash/docs/ClassCommand.kt
@@ -1,61 +1,55 @@
-package com.freya02.bot.commands.slash.docs;
+package com.freya02.bot.commands.slash.docs
 
-import com.freya02.bot.docs.index.DocIndex;
-import com.freya02.botcommands.api.application.annotations.AppOption;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
-import com.freya02.docs.DocSourceType;
-import org.jetbrains.annotations.NotNull;
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.application.annotations.AppOption
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand
+import com.freya02.docs.DocSourceType
 
-import java.io.IOException;
+@CommandMarker
+class ClassCommand : BaseDocCommand() {
+    @JDASlashCommand(name = "class", subcommand = "botcommands", description = "Shows the documentation for a class")
+    fun onSlashClassBC(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Name of the Java class",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_AUTOCOMPLETE_NAME
+        ) className: String
+    ) {
+        onSlashClass(event, sourceType, className)
+    }
 
-public class ClassCommand extends BaseDocCommand {
-	public ClassCommand() throws IOException {}
+    @JDASlashCommand(name = "class", subcommand = "jda", description = "Shows the documentation for a class")
+    fun onSlashClassJDA(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Name of the Java class",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_AUTOCOMPLETE_NAME
+        ) className: String
+    ) {
+        onSlashClass(event, sourceType, className)
+    }
 
-	@JDASlashCommand(
-			name = "class",
-			subcommand = "botcommands",
-			description = "Shows the documentation for a class"
-	)
-	public void onSlashClassBC(@NotNull GuildSlashEvent event,
-	                           @NotNull @AppOption(description = "The docs to search upon")
-			                           DocSourceType sourceType,
-	                           @NotNull @AppOption(description = "Name of the Java class", autocomplete = CommonDocsHandlers.CLASS_NAME_AUTOCOMPLETE_NAME)
-			                           String className) throws IOException {
-		onSlashClass(event, sourceType, className);
-	}
+    @JDASlashCommand(name = "class", subcommand = "java", description = "Shows the documentation for a class")
+    fun onSlashClassJava(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Name of the Java class",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_AUTOCOMPLETE_NAME
+        ) className: String
+    ) {
+        onSlashClass(event, sourceType, className)
+    }
 
-	@JDASlashCommand(
-			name = "class",
-			subcommand = "jda",
-			description = "Shows the documentation for a class"
-	)
-	public void onSlashClassJDA(@NotNull GuildSlashEvent event,
-	                            @NotNull @AppOption(description = "The docs to search upon")
-			                            DocSourceType sourceType,
-	                            @NotNull @AppOption(description = "Name of the Java class", autocomplete = CommonDocsHandlers.CLASS_NAME_AUTOCOMPLETE_NAME)
-			                            String className) throws IOException {
-		onSlashClass(event, sourceType, className);
-	}
-
-	@JDASlashCommand(
-			name = "class",
-			subcommand = "java",
-			description = "Shows the documentation for a class"
-	)
-	public void onSlashClassJava(@NotNull GuildSlashEvent event,
-	                             @NotNull @AppOption(description = "The docs to search upon")
-			                             DocSourceType sourceType,
-	                             @NotNull @AppOption(description = "Name of the Java class", autocomplete = CommonDocsHandlers.CLASS_NAME_AUTOCOMPLETE_NAME)
-			                             String className) throws IOException {
-		onSlashClass(event, sourceType, className);
-	}
-
-	private void onSlashClass(@NotNull GuildSlashEvent event,
-	                          @NotNull DocSourceType sourceType,
-	                          @NotNull String className) throws IOException {
-		final DocIndex docIndex = docIndexMap.get(sourceType);
-
-		CommonDocsHandlers.handleClass(event, className, docIndex);
-	}
+    private fun onSlashClass(
+        event: GuildSlashEvent,
+        sourceType: DocSourceType,
+        className: String
+    ) {
+        val docIndex = docIndexMap[sourceType]
+        CommonDocsHandlers.handleClass(event, className, docIndex)
+    }
 }
