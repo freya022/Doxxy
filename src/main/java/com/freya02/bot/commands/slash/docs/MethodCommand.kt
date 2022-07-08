a1577190e5ea85365d85a046c53c5ac68d13f74c
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-08 16:01:27 +0200

    Convert MethodCommand

--- src/main/java/com/freya02/bot/commands/slash/docs/MethodCommand.kt
+++ src/main/java/com/freya02/bot/commands/slash/docs/MethodCommand.kt
@@ -1,69 +1,80 @@
-package com.freya02.bot.commands.slash.docs;
+package com.freya02.bot.commands.slash.docs
 
-import com.freya02.bot.docs.index.DocIndex;
-import com.freya02.botcommands.api.application.annotations.AppOption;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
-import com.freya02.docs.DocSourceType;
-import org.jetbrains.annotations.NotNull;
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.application.annotations.AppOption
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand
+import com.freya02.docs.DocSourceType
 
-import java.io.IOException;
+@CommandMarker
+class MethodCommand : BaseDocCommand() {
+    @JDASlashCommand(
+        name = "method",
+        subcommand = "botcommands",
+        description = "Shows the documentation for a method of a class"
+    )
+    fun onSlashMethodBC(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Name of the Java class",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME
+        ) className: String,
+        @AppOption(
+            description = "Signature of the method",
+            autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
+        ) methodId: String
+    ) {
+        onSlashMethod(event, sourceType, className, methodId)
+    }
 
-public class MethodCommand extends BaseDocCommand {
-	public MethodCommand() throws IOException {}
+    @JDASlashCommand(
+        name = "method",
+        subcommand = "jda",
+        description = "Shows the documentation for a method of a class"
+    )
+    fun onSlashMethodJDA(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Name of the Java class",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME
+        ) className: String,
+        @AppOption(
+            description = "Signature of the method",
+            autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
+        ) methodId: String
+    ) {
+        onSlashMethod(event, sourceType, className, methodId)
+    }
 
-	@JDASlashCommand(
-			name = "method",
-			subcommand = "botcommands",
-			description = "Shows the documentation for a method of a class"
-	)
-	public void onSlashMethodBC(@NotNull GuildSlashEvent event,
-	                            @NotNull @AppOption(description = "The docs to search upon")
-			                            DocSourceType sourceType,
-	                            @NotNull @AppOption(description = "Name of the Java class", autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME)
-			                            String className,
-	                            @NotNull @AppOption(description = "Signature of the method", autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME)
-			                            String methodId) throws IOException {
-		onSlashMethod(event, sourceType, className, methodId);
-	}
+    @JDASlashCommand(
+        name = "method",
+        subcommand = "java",
+        description = "Shows the documentation for a method of a class"
+    )
+    fun onSlashMethodJava(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Name of the Java class",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME
+        ) className: String,
+        @AppOption(
+            description = "Signature of the method",
+            autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
+        ) methodId: String
+    ) {
+        onSlashMethod(event, sourceType, className, methodId)
+    }
 
-	@JDASlashCommand(
-			name = "method",
-			subcommand = "jda",
-			description = "Shows the documentation for a method of a class"
-	)
-	public void onSlashMethodJDA(@NotNull GuildSlashEvent event,
-	                             @NotNull @AppOption(description = "The docs to search upon")
-			                             DocSourceType sourceType,
-	                             @NotNull @AppOption(description = "Name of the Java class", autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME)
-			                             String className,
-	                             @NotNull @AppOption(description = "Signature of the method", autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME)
-			                             String methodId) throws IOException {
-		onSlashMethod(event, sourceType, className, methodId);
-	}
-
-	@JDASlashCommand(
-			name = "method",
-			subcommand = "java",
-			description = "Shows the documentation for a method of a class"
-	)
-	public void onSlashMethodJava(@NotNull GuildSlashEvent event,
-	                              @NotNull @AppOption(description = "The docs to search upon")
-			                              DocSourceType sourceType,
-	                              @NotNull @AppOption(description = "Name of the Java class", autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME)
-			                              String className,
-	                              @NotNull @AppOption(description = "Signature of the method", autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME)
-			                              String methodId) throws IOException {
-		onSlashMethod(event, sourceType, className, methodId);
-	}
-
-	private void onSlashMethod(@NotNull GuildSlashEvent event,
-	                           @NotNull DocSourceType sourceType,
-	                           @NotNull String className,
-	                           @NotNull String methodId) throws IOException {
-
-		final DocIndex docIndex = docIndexMap.get(sourceType);
-
-		CommonDocsHandlers.handleMethodDocs(event, className, methodId, docIndex);
-	}
+    private fun onSlashMethod(
+        event: GuildSlashEvent,
+        sourceType: DocSourceType,
+        className: String,
+        methodId: String
+    ) {
+        val docIndex = docIndexMap[sourceType]
+        CommonDocsHandlers.handleMethodDocs(event, className, methodId, docIndex)
+    }
 }
