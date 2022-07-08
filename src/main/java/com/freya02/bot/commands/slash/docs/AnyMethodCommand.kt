422eee470a42769006e0c7d5c02a8a7aff50fae3
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-08 15:58:50 +0200

    Convert AnyMethodCommand

--- src/main/java/com/freya02/bot/commands/slash/docs/AnyMethodCommand.kt
+++ src/main/java/com/freya02/bot/commands/slash/docs/AnyMethodCommand.kt
@@ -1,69 +1,69 @@
-package com.freya02.bot.commands.slash.docs;
+package com.freya02.bot.commands.slash.docs
 
-import com.freya02.bot.docs.index.DocIndex;
-import com.freya02.botcommands.api.application.annotations.AppOption;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
-import com.freya02.docs.DocSourceType;
-import org.jetbrains.annotations.NotNull;
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.application.annotations.AppOption
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand
+import com.freya02.docs.DocSourceType
+import dev.minn.jda.ktx.messages.reply_
 
-import java.io.IOException;
+@CommandMarker
+class AnyMethodCommand : BaseDocCommand() {
+    @JDASlashCommand(
+        name = "anymethod",
+        subcommand = "botcommands",
+        description = "Shows the documentation for any method"
+    )
+    fun onSlashAnyMethodBC(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Full signature of the class + method",
+            autocomplete = CommonDocsHandlers.ANY_METHOD_NAME_AUTOCOMPLETE_NAME
+        ) fullSignature: String
+    ) {
+        onSlashAnyMethod(event, sourceType, fullSignature)
+    }
 
-public class AnyMethodCommand extends BaseDocCommand {
-	public AnyMethodCommand() throws IOException {}
+    @JDASlashCommand(name = "anymethod", subcommand = "jda", description = "Shows the documentation for any method")
+    fun onSlashAnyMethodJDA(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Full signature of the class + method",
+            autocomplete = CommonDocsHandlers.ANY_METHOD_NAME_AUTOCOMPLETE_NAME
+        ) fullSignature: String
+    ) {
+        onSlashAnyMethod(event, sourceType, fullSignature)
+    }
 
-	@JDASlashCommand(
-			name = "anymethod",
-			subcommand = "botcommands",
-			description = "Shows the documentation for any method"
-	)
-	public void onSlashAnyMethodBC(@NotNull GuildSlashEvent event,
-	                               @NotNull @AppOption(description = "The docs to search upon")
-			                               DocSourceType sourceType,
-	                               @NotNull @AppOption(description = "Full signature of the class + method", autocomplete = CommonDocsHandlers.ANY_METHOD_NAME_AUTOCOMPLETE_NAME)
-			                               String fullSignature) throws IOException {
-		onSlashAnyMethod(event, sourceType, fullSignature);
-	}
+    @JDASlashCommand(name = "anymethod", subcommand = "java", description = "Shows the documentation for any method")
+    fun onSlashAnyMethodJava(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "Full signature of the class + method",
+            autocomplete = CommonDocsHandlers.ANY_METHOD_NAME_AUTOCOMPLETE_NAME
+        ) fullSignature: String
+    ) {
+        onSlashAnyMethod(event, sourceType, fullSignature)
+    }
 
-	@JDASlashCommand(
-			name = "anymethod",
-			subcommand = "jda",
-			description = "Shows the documentation for any method"
-	)
-	public void onSlashAnyMethodJDA(@NotNull GuildSlashEvent event,
-	                                @NotNull @AppOption(description = "The docs to search upon")
-			                                DocSourceType sourceType,
-	                                @NotNull @AppOption(description = "Full signature of the class + method", autocomplete = CommonDocsHandlers.ANY_METHOD_NAME_AUTOCOMPLETE_NAME)
-			                                String fullSignature) throws IOException {
-		onSlashAnyMethod(event, sourceType, fullSignature);
-	}
+    private fun onSlashAnyMethod(
+        event: GuildSlashEvent,
+        sourceType: DocSourceType,
+        fullSignature: String
+    ) {
+        val split = fullSignature.split("#".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        if (split.size != 2) {
+            event.reply_(
+                "You must supply a class name and a method signature, separated with a #, e.g. `Object#getClass()`",
+                ephemeral = true
+            ).queue()
+            return
+        }
 
-	@JDASlashCommand(
-			name = "anymethod",
-			subcommand = "java",
-			description = "Shows the documentation for any method"
-	)
-	public void onSlashAnyMethodJava(@NotNull GuildSlashEvent event,
-	                                 @NotNull @AppOption(description = "The docs to search upon")
-			                                 DocSourceType sourceType,
-	                                 @NotNull @AppOption(description = "Full signature of the class + method", autocomplete = CommonDocsHandlers.ANY_METHOD_NAME_AUTOCOMPLETE_NAME)
-			                                 String fullSignature) throws IOException {
-		onSlashAnyMethod(event, sourceType, fullSignature);
-	}
-
-	private void onSlashAnyMethod(@NotNull GuildSlashEvent event,
-	                              @NotNull DocSourceType sourceType,
-	                              @NotNull String fullSignature) throws IOException {
-
-		final String[] split = fullSignature.split("#");
-		if (split.length != 2) {
-			event.reply("You must supply a class name and a method signature, separated with a #, e.g. `Object#getClass()`").setEphemeral(true).queue();
-
-			return;
-		}
-
-		final DocIndex docIndex = docIndexMap.get(sourceType);
-
-		CommonDocsHandlers.handleMethodDocs(event, split[0], split[1], docIndex);
-	}
+        val docIndex = docIndexMap[sourceType]
+        CommonDocsHandlers.handleMethodDocs(event, split[0], split[1], docIndex)
+    }
 }
