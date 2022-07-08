267652c5e9a4e0ece14c06b2ae472ff62e50b137
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-08 16:00:39 +0200

    Convert FieldCommand

--- src/main/java/com/freya02/bot/commands/slash/docs/FieldCommand.kt
+++ src/main/java/com/freya02/bot/commands/slash/docs/FieldCommand.kt
@@ -1,69 +1,76 @@
-package com.freya02.bot.commands.slash.docs;
+package com.freya02.bot.commands.slash.docs
 
-import com.freya02.bot.docs.index.DocIndex;
-import com.freya02.botcommands.api.application.annotations.AppOption;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
-import com.freya02.docs.DocSourceType;
-import org.jetbrains.annotations.NotNull;
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.application.annotations.AppOption
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand
+import com.freya02.docs.DocSourceType
 
-import java.io.IOException;
+@CommandMarker
+class FieldCommand : BaseDocCommand() {
+    @JDASlashCommand(
+        name = "field",
+        subcommand = "botcommands",
+        description = "Shows the documentation for a field of a class"
+    )
+    fun onSlashFieldBC(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "The class to search the field in",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME
+        ) className: String,
+        @AppOption(
+            description = "Name of the field",
+            autocomplete = CommonDocsHandlers.FIELD_NAME_AUTOCOMPLETE_NAME
+        ) fieldName: String
+    ) {
+        onSlashField(event, sourceType, className, fieldName)
+    }
 
-public class FieldCommand extends BaseDocCommand {
-	public FieldCommand() throws IOException {}
+    @JDASlashCommand(name = "field", subcommand = "jda", description = "Shows the documentation for a field of a class")
+    fun onSlashFieldJDA(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "The class to search the field in",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME
+        ) className: String,
+        @AppOption(
+            description = "Name of the field",
+            autocomplete = CommonDocsHandlers.FIELD_NAME_AUTOCOMPLETE_NAME
+        ) fieldName: String
+    ) {
+        onSlashField(event, sourceType, className, fieldName)
+    }
 
-	@JDASlashCommand(
-			name = "field",
-			subcommand = "botcommands",
-			description = "Shows the documentation for a field of a class"
-	)
-	public void onSlashFieldBC(@NotNull GuildSlashEvent event,
-	                           @NotNull @AppOption(description = "The docs to search upon")
-			                           DocSourceType sourceType,
-	                           @NotNull @AppOption(description = "The class to search the field in", autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME)
-			                           String className,
-	                           @NotNull @AppOption(description = "Name of the field", autocomplete = CommonDocsHandlers.FIELD_NAME_AUTOCOMPLETE_NAME)
-			                           String fieldName) throws IOException {
-		onSlashField(event, sourceType, className, fieldName);
-	}
+    @JDASlashCommand(
+        name = "field",
+        subcommand = "java",
+        description = "Shows the documentation for a field of a class"
+    )
+    fun onSlashFieldJava(
+        event: GuildSlashEvent,
+        @AppOption(description = "The docs to search upon") sourceType: DocSourceType,
+        @AppOption(
+            description = "The class to search the field in",
+            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME
+        ) className: String,
+        @AppOption(
+            description = "Name of the field",
+            autocomplete = CommonDocsHandlers.FIELD_NAME_AUTOCOMPLETE_NAME
+        ) fieldName: String
+    ) {
+        onSlashField(event, sourceType, className, fieldName)
+    }
 
-	@JDASlashCommand(
-			name = "field",
-			subcommand = "jda",
-			description = "Shows the documentation for a field of a class"
-	)
-	public void onSlashFieldJDA(@NotNull GuildSlashEvent event,
-	                            @NotNull @AppOption(description = "The docs to search upon")
-			                            DocSourceType sourceType,
-	                            @NotNull @AppOption(description = "The class to search the field in", autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME)
-			                            String className,
-	                            @NotNull @AppOption(description = "Name of the field", autocomplete = CommonDocsHandlers.FIELD_NAME_AUTOCOMPLETE_NAME)
-			                            String fieldName) throws IOException {
-		onSlashField(event, sourceType, className, fieldName);
-	}
-
-	@JDASlashCommand(
-			name = "field",
-			subcommand = "java",
-			description = "Shows the documentation for a field of a class"
-	)
-	public void onSlashFieldJava(@NotNull GuildSlashEvent event,
-	                             @NotNull @AppOption(description = "The docs to search upon")
-			                             DocSourceType sourceType,
-	                             @NotNull @AppOption(description = "The class to search the field in", autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME)
-			                             String className,
-	                             @NotNull @AppOption(description = "Name of the field", autocomplete = CommonDocsHandlers.FIELD_NAME_AUTOCOMPLETE_NAME)
-			                             String fieldName) throws IOException {
-		onSlashField(event, sourceType, className, fieldName);
-	}
-
-	private void onSlashField(@NotNull GuildSlashEvent event,
-	                          @NotNull DocSourceType sourceType,
-	                          @NotNull String className,
-	                          @NotNull String fieldName) throws IOException {
-
-		final DocIndex docIndex = docIndexMap.get(sourceType);
-
-		CommonDocsHandlers.handleFieldDocs(event, className, fieldName, docIndex);
-	}
+    private fun onSlashField(
+        event: GuildSlashEvent,
+        sourceType: DocSourceType,
+        className: String,
+        fieldName: String
+    ) {
+        val docIndex = docIndexMap[sourceType]
+        CommonDocsHandlers.handleFieldDocs(event, className, fieldName, docIndex)
+    }
 }
