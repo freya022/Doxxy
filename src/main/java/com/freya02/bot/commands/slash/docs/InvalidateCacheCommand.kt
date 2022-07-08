ee7837929073e03efa9c077936c3b854738f3b0a
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-08 16:02:14 +0200

    Convert InvalidateCacheCommand

--- src/main/java/com/freya02/bot/commands/slash/docs/InvalidateCacheCommand.kt
+++ src/main/java/com/freya02/bot/commands/slash/docs/InvalidateCacheCommand.kt
@@ -1,32 +1,26 @@
-package com.freya02.bot.commands.slash.docs;
+package com.freya02.bot.commands.slash.docs
 
-import com.freya02.bot.docs.DocIndexMap;
-import com.freya02.botcommands.api.application.ApplicationCommand;
-import com.freya02.botcommands.api.application.annotations.Test;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
-import com.freya02.docs.DocSourceType;
+import com.freya02.bot.docs.DocIndexMap
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.application.ApplicationCommand
+import com.freya02.botcommands.api.application.annotations.Test
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand
+import com.freya02.docs.DocSourceType
 
-import java.io.IOException;
+@CommandMarker
+class InvalidateCacheCommand : ApplicationCommand() {
+    @Test
+    @JDASlashCommand(name = "invalidate")
+    fun onSlashInvalidate(event: GuildSlashEvent) {
+        event.deferReply(true).queue()
+        DocIndexMap.refreshAndInvalidateIndex(DocSourceType.BOT_COMMANDS)
+        DocIndexMap.refreshAndInvalidateIndex(DocSourceType.JDA)
+        //		DocIndexMap.refreshAndInvalidateIndex(DocSourceType.JAVA); Java's docs dont get updated lol
 
-import static com.freya02.bot.commands.slash.docs.CommonDocsHandlers.AUTOCOMPLETE_NAMES;
-
-public class InvalidateCacheCommand extends ApplicationCommand {
-	@Test
-	@JDASlashCommand(
-			name = "invalidate"
-	)
-	public void onSlashInvalidate(GuildSlashEvent event) throws IOException {
-		event.deferReply(true).queue();
-
-		DocIndexMap.refreshAndInvalidateIndex(DocSourceType.BOT_COMMANDS);
-		DocIndexMap.refreshAndInvalidateIndex(DocSourceType.JDA);
-//		DocIndexMap.refreshAndInvalidateIndex(DocSourceType.JAVA); Java's docs dont get updated lol
-
-		for (String autocompleteName : AUTOCOMPLETE_NAMES) {
-			event.getContext().invalidateAutocompletionCache(autocompleteName);
-		}
-
-		event.getHook().sendMessage("Done").queue();
-	}
+        for (autocompleteName in CommonDocsHandlers.AUTOCOMPLETE_NAMES) {
+            event.context.invalidateAutocompletionCache(autocompleteName)
+        }
+        event.hook.sendMessage("Done").queue()
+    }
 }
