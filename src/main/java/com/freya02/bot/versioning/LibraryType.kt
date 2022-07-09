0eef7c54668ad0726a4600c9e3d45e9ce0cc33a0
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-09 16:43:07 +0200

    Convert LibraryType

--- src/main/java/com/freya02/bot/versioning/LibraryType.kt
+++ src/main/java/com/freya02/bot/versioning/LibraryType.kt
@@ -1,17 +1,7 @@
-package com.freya02.bot.versioning;
+package com.freya02.bot.versioning
 
-public enum LibraryType {
-	JDA4("JDA 4"),
-	JDA5("JDA 5"),
-	BOT_COMMANDS("BotCommands");
-
-	private final String displayString;
-
-	LibraryType(String displayString) {
-		this.displayString = displayString;
-	}
-
-	public String getDisplayString() {
-		return displayString;
-	}
+enum class LibraryType(val displayString: String) {
+    JDA4("JDA 4"),
+    JDA5("JDA 5"),
+    BOT_COMMANDS("BotCommands");
 }
