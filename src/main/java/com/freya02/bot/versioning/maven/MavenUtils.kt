4c3e3d00ad2b0e66ff7d9573d89004a15b8d0a83
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-09 16:42:19 +0200

    Convert MavenUtils

--- src/main/java/com/freya02/bot/versioning/maven/MavenUtils.kt
+++ src/main/java/com/freya02/bot/versioning/maven/MavenUtils.kt
@@ -1,79 +1,60 @@
-package com.freya02.bot.versioning.maven;
+package com.freya02.bot.versioning.maven
 
-import com.freya02.bot.utils.HttpUtils;
-import com.freya02.bot.versioning.ArtifactInfo;
-import net.dv8tion.jda.api.exceptions.ParsingException;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
-import org.jetbrains.annotations.NotNull;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.freya02.bot.utils.HttpUtils
+import com.freya02.bot.versioning.ArtifactInfo
+import net.dv8tion.jda.api.exceptions.ParsingException
+import org.jsoup.nodes.Document
+import java.io.IOException
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Objects;
+object MavenUtils {
+    @Throws(IOException::class)
+    fun getLatestMavenVersion(formatUrl: String, groupId: String, artifactId: String?): String {
+        return getMavenMetadata(formatUrl, groupId, artifactId)
+            .selectFirst("metadata > versioning > latest")
+            ?.text() ?: throw ParsingException("Unable to parse latest version")
+    }
 
-public class MavenUtils {
-	public static final String MAVEN_JAVADOC_FORMAT = "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s-javadoc.jar";
+    @Throws(IOException::class)
+    fun retrieveDependencyVersion(
+        ownerName: String,
+        artifactId: String,
+        branchName: String,
+        targetArtifactId: String
+    ): ArtifactInfo {
+        val document = HttpUtils.getDocument(
+            "https://raw.githubusercontent.com/%s/%s/%s/pom.xml".format(
+                ownerName,
+                artifactId,
+                branchName
+            )
+        )
 
-	@NotNull
-	public static String getLatestMavenVersion(String formatUrl, String groupId, String artifactId) throws IOException {
-		final Document document = getMavenMetadata(formatUrl, groupId, artifactId);
+        document.select("project > dependencies > dependency")
+            .forEachIndexed { i, element ->
+                val groupIdElement = element.selectFirst("groupId")
+                val artifactIdElement = element.selectFirst("artifactId")
+                val versionElement = element.selectFirst("version")
 
-		final Element latestElement = document.selectFirst("metadata > versioning > latest");
-		if (latestElement == null) throw new ParsingException("Unable to parse latest version");
+                if (groupIdElement == null || artifactIdElement == null || versionElement == null) {
+                    throw IllegalArgumentException("Could not parse dependency #$i at ${document.baseUri()}")
+                }
 
-		return latestElement.text();
-	}
+                if (artifactIdElement.text() != targetArtifactId) {
+                    return@forEachIndexed
+                }
 
-	@NotNull
-	public static Document getMavenMetadata(String formatUrl, String groupId, String artifactId) throws IOException {
-		return HttpUtils.getDocument(formatUrl.formatted(groupId.replace('.', '/'), artifactId));
-	}
+                return ArtifactInfo(
+                    groupIdElement.text(),
+                    artifactIdElement.text(),
+                    versionElement.text()
+                )
+            }
 
-	public static void downloadMavenDocs(@NotNull ArtifactInfo artifactInfo, @NotNull Path targetPath) throws IOException {
-		try (Response response = HttpUtils.CLIENT.newCall(new Request.Builder()
-						.url(MAVEN_JAVADOC_FORMAT.formatted(artifactInfo.getGroupId().replace('.', '/'),
-								artifactInfo.getArtifactId(),
-								artifactInfo.getVersion(),
-								artifactInfo.getArtifactId(),
-								artifactInfo.getVersion()))
-						.build())
-				.execute()) {
-			final ResponseBody body = response.body();
-			if (body == null) throw new IOException("Got no ResponseBody for " + response.request().url());
+        throw IOException("Unable to get dependency version from " + document.baseUri())
+    }
 
-			if (!response.isSuccessful()) throw new IOException("Got an unsuccessful response from " + response.request().url() + ", code: " + response.code());
-
-			Files.copy(body.byteStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
-		}
-	}
-
-	@NotNull
-	public static ArtifactInfo retrieveDependencyVersion(String ownerName, String artifactId, String branchName, String targetArtifactId) throws IOException {
-		final Document document = HttpUtils.getDocument("https://raw.githubusercontent.com/%s/%s/%s/pom.xml".formatted(ownerName, artifactId, branchName));
-
-		Elements elements = document.select("project > dependencies > dependency");
-		for (int i = 0, selectSize = elements.size(); i < selectSize; i++) {
-			final Element element = elements.get(i);
-
-			final Element groupIdElement = Objects.requireNonNull(element.selectFirst("groupId"), "Could not parse dependency #" + i + " at " + document.baseUri());
-			final Element artifactIdElement = Objects.requireNonNull(element.selectFirst("artifactId"), "Could not parse dependency #" + i + " at " + document.baseUri());
-			final Element versionElement = Objects.requireNonNull(element.selectFirst("version"), "Could not parse dependency #" + i + " at " + document.baseUri());
-
-			if (!artifactIdElement.text().equals(targetArtifactId)) continue;
-
-			return new ArtifactInfo(
-					groupIdElement.text(),
-					artifactIdElement.text(),
-					versionElement.text()
-			);
-		}
-
-		throw new IOException("Unable to get dependency version from " + document.baseUri());
-	}
+    @Throws(IOException::class)
+    private fun getMavenMetadata(formatUrl: String, groupId: String, artifactId: String?): Document {
+        return HttpUtils.getDocument(formatUrl.formatted(groupId.replace('.', '/'), artifactId))
+    }
 }
