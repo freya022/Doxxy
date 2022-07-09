e1796e1ad7b361d8a903e2c830846c0c7a278820
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-09 15:43:09 +0200

    Convert LibraryTypeResolver, DocSourceTypeResolver

--- src/main/java/com/freya02/bot/docs/DocSourceTypeResolver.kt
+++ src/main/java/com/freya02/bot/docs/DocSourceTypeResolver.kt
@@ -1,54 +1,38 @@
-package com.freya02.bot.docs;
+package com.freya02.bot.docs
 
-import com.freya02.botcommands.api.BContext;
-import com.freya02.botcommands.api.parameters.ParameterResolver;
-import com.freya02.botcommands.api.parameters.SlashParameterResolver;
-import com.freya02.botcommands.internal.application.slash.SlashCommandInfo;
-import com.freya02.docs.DocSourceType;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.interactions.commands.Command;
-import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.freya02.bot.utils.Utils.isBCGuild
+import com.freya02.botcommands.api.BContext
+import com.freya02.botcommands.api.parameters.ParameterResolver
+import com.freya02.botcommands.api.parameters.SlashParameterResolver
+import com.freya02.botcommands.internal.application.slash.SlashCommandInfo
+import com.freya02.docs.DocSourceType
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload
+import net.dv8tion.jda.api.interactions.commands.OptionMapping
+import net.dv8tion.jda.api.interactions.commands.OptionType
 
-import java.util.Collection;
-import java.util.List;
+class DocSourceTypeResolver : ParameterResolver(DocSourceType::class.java), SlashParameterResolver {
+    override fun getOptionType(): OptionType = OptionType.STRING
 
-import static com.freya02.bot.utils.Utils.isBCGuild;
+    override fun resolve(
+        context: BContext,
+        info: SlashCommandInfo,
+        event: CommandInteractionPayload,
+        optionMapping: OptionMapping
+    ): Any = DocSourceType.valueOf(optionMapping.asString)
 
-public class DocSourceTypeResolver extends ParameterResolver implements SlashParameterResolver {
-	public DocSourceTypeResolver() {
-		super(DocSourceType.class);
-	}
-
-	@Override
-	@NotNull
-	public OptionType getOptionType() {
-		return OptionType.STRING;
-	}
-
-	@Override
-	@Nullable
-	public Object resolve(@NotNull BContext context, @NotNull SlashCommandInfo info, @NotNull CommandInteractionPayload event, @NotNull OptionMapping optionMapping) {
-		return DocSourceType.valueOf(optionMapping.getAsString());
-	}
-
-	@Override
-	@NotNull
-	public Collection<Command.Choice> getPredefinedChoices(@Nullable Guild guild) {
-		if (isBCGuild(guild)) {
-			return List.of(
-					new Command.Choice("BotCommands", DocSourceType.BOT_COMMANDS.name()),
-					new Command.Choice("JDA", DocSourceType.JDA.name()),
-					new Command.Choice("Java", DocSourceType.JAVA.name())
-			);
-		}
-
-		return List.of(
-				new Command.Choice("JDA", DocSourceType.JDA.name()),
-				new Command.Choice("Java", DocSourceType.JAVA.name())
-		);
-	}
+    override fun getPredefinedChoices(guild: Guild?): Collection<Command.Choice> {
+        return when {
+            guild.isBCGuild() -> listOf(
+                Command.Choice("BotCommands", DocSourceType.BOT_COMMANDS.name),
+                Command.Choice("JDA", DocSourceType.JDA.name),
+                Command.Choice("Java", DocSourceType.JAVA.name)
+            )
+            else -> listOf(
+                Command.Choice("JDA", DocSourceType.JDA.name),
+                Command.Choice("Java", DocSourceType.JAVA.name)
+            )
+        }
+    }
 }
--- src/main/java/com/freya02/bot/versioning/LibraryTypeResolver.kt
+++ src/main/java/com/freya02/bot/versioning/LibraryTypeResolver.kt
@@ -1,53 +1,39 @@
-package com.freya02.bot.versioning;
+package com.freya02.bot.versioning
 
-import com.freya02.botcommands.api.BContext;
-import com.freya02.botcommands.api.parameters.ParameterResolver;
-import com.freya02.botcommands.api.parameters.SlashParameterResolver;
-import com.freya02.botcommands.internal.application.slash.SlashCommandInfo;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.interactions.commands.Command;
-import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.freya02.bot.utils.Utils.isBCGuild
+import com.freya02.botcommands.api.BContext
+import com.freya02.botcommands.api.parameters.ParameterResolver
+import com.freya02.botcommands.api.parameters.SlashParameterResolver
+import com.freya02.botcommands.internal.application.slash.SlashCommandInfo
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload
+import net.dv8tion.jda.api.interactions.commands.OptionMapping
+import net.dv8tion.jda.api.interactions.commands.OptionType
 
-import java.util.Collection;
-import java.util.List;
+class LibraryTypeResolver : ParameterResolver(LibraryType::class.java), SlashParameterResolver {
+    override fun getOptionType(): OptionType {
+        return OptionType.STRING
+    }
 
-import static com.freya02.bot.utils.Utils.isBCGuild;
+    override fun getPredefinedChoices(guild: Guild?): Collection<Command.Choice> {
+        return when {
+            guild.isBCGuild() -> listOf(
+                Command.Choice("BotCommands", LibraryType.BOT_COMMANDS.name),
+                Command.Choice("JDA 5", LibraryType.JDA5.name),
+                Command.Choice("JDA 4", LibraryType.JDA4.name)
+            )
+            else -> listOf(
+                Command.Choice("JDA 5", LibraryType.JDA5.name),
+                Command.Choice("JDA 4", LibraryType.JDA4.name)
+            )
+        }
+    }
 
-public class LibraryTypeResolver extends ParameterResolver implements SlashParameterResolver {
-	public LibraryTypeResolver() {
-		super(LibraryType.class);
-	}
-
-	@Override
-	@NotNull
-	public OptionType getOptionType() {
-		return OptionType.STRING;
-	}
-
-	@Override
-	@NotNull
-	public Collection<Command.Choice> getPredefinedChoices(@Nullable Guild guild) {
-		if (isBCGuild(guild)) {
-			return List.of(
-					new Command.Choice("BotCommands", LibraryType.BOT_COMMANDS.name()),
-					new Command.Choice("JDA 5", LibraryType.JDA5.name()),
-					new Command.Choice("JDA 4", LibraryType.JDA4.name())
-			);
-		}
-
-		return List.of(
-				new Command.Choice("JDA 5", LibraryType.JDA5.name()),
-				new Command.Choice("JDA 4", LibraryType.JDA4.name())
-		);
-	}
-
-	@Override
-	@Nullable
-	public Object resolve(@NotNull BContext context, @NotNull SlashCommandInfo info, @NotNull CommandInteractionPayload event, @NotNull OptionMapping optionMapping) {
-		return LibraryType.valueOf(optionMapping.getAsString());
-	}
+    override fun resolve(
+        context: BContext,
+        info: SlashCommandInfo,
+        event: CommandInteractionPayload,
+        optionMapping: OptionMapping
+    ): Any = LibraryType.valueOf(optionMapping.asString)
 }
