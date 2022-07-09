df588ea6bfdd1f51365f27e5b606bba1d3bc9264
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-09 16:45:01 +0200

    Convert RepoType

--- src/main/java/com/freya02/bot/versioning/maven/RepoType.kt
+++ src/main/java/com/freya02/bot/versioning/maven/RepoType.kt
@@ -1,16 +1,6 @@
-package com.freya02.bot.versioning.maven;
+package com.freya02.bot.versioning.maven
 
-public enum RepoType {
-	M2("https://m2.dv8tion.net/releases/%s/%s/maven-metadata.xml"),
-	MAVEN("https://repo.maven.apache.org/maven2/%s/%s/maven-metadata.xml");
-
-	private final String urlFormat;
-
-	RepoType(String urlFormat) {
-		this.urlFormat = urlFormat;
-	}
-
-	public String getUrlFormat() {
-		return urlFormat;
-	}
+enum class RepoType(val urlFormat: String) {
+    M2("https://m2.dv8tion.net/releases/%s/%s/maven-metadata.xml"),
+    MAVEN("https://repo.maven.apache.org/maven2/%s/%s/maven-metadata.xml");
 }
