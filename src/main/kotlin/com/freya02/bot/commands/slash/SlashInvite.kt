95f849c52b984b9f7bee15197fe0b25df8e7feba
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-15 15:55:12 +0200

    Move kotlin source to kotlin folder

--- src/main/kotlin/com/freya02/bot/commands/slash/SlashInvite.kt
+++ src/main/kotlin/com/freya02/bot/commands/slash/SlashInvite.kt
@@ -1,16 +1,14 @@
-package com.freya02.bot.commands.text;
+package com.freya02.bot.commands.slash
 
-import com.freya02.botcommands.api.application.ApplicationCommand;
-import com.freya02.botcommands.api.application.annotations.Test;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import com.freya02.botcommands.api.application.ApplicationCommand
+import com.freya02.botcommands.api.application.annotations.Test
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand
 
-public class SlashInvite extends ApplicationCommand {
-	@Test
-	@JDASlashCommand(
-			name = "invite"
-	)
-	public void onSlashInvite(GuildSlashEvent event) {
-		event.reply(event.getJDA().getInviteUrl()).queue();
-	}
+class SlashInvite : ApplicationCommand() {
+    @Test
+    @JDASlashCommand(name = "invite")
+    fun onSlashInvite(event: GuildSlashEvent) {
+        event.reply(event.jda.getInviteUrl()).queue()
+    }
 }
--- src/main/kotlin/com/freya02/bot/commands/text/Exit.kt
+++ src/main/kotlin/com/freya02/bot/commands/text/Exit.kt
@@ -1,31 +1,28 @@
-package com.freya02.bot.commands.text;
+package com.freya02.bot.commands.text
 
-import com.freya02.botcommands.api.Logging;
-import com.freya02.botcommands.api.annotations.RequireOwner;
-import com.freya02.botcommands.api.prefixed.BaseCommandEvent;
-import com.freya02.botcommands.api.prefixed.TextCommand;
-import com.freya02.botcommands.api.prefixed.annotations.JDATextCommand;
-import net.dv8tion.jda.api.JDA;
-import org.slf4j.Logger;
+import com.freya02.botcommands.api.Logging
+import com.freya02.botcommands.api.annotations.RequireOwner
+import com.freya02.botcommands.api.prefixed.BaseCommandEvent
+import com.freya02.botcommands.api.prefixed.TextCommand
+import com.freya02.botcommands.api.prefixed.annotations.JDATextCommand
+import net.dv8tion.jda.api.JDA
+import kotlin.system.exitProcess
 
-public class Exit extends TextCommand {
-	private static final Logger LOGGER = Logging.getLogger();
+class Exit : TextCommand() {
+    private val logger = Logging.getLogger()
 
-	@RequireOwner
-	@JDATextCommand(name = "exit")
-	public void say(BaseCommandEvent event) throws InterruptedException {
-		LOGGER.warn("Shutdown initiated by {} ({})", event.getAuthor().getAsTag(), event.getAuthor().getId());
+    @RequireOwner
+    @JDATextCommand(name = "exit")
+    fun exit(event: BaseCommandEvent) {
+        logger.warn("Shutdown initiated by {} ({})", event.author.asTag, event.author.id)
+        event.reactSuccess().mapToResult().complete()
 
-		event.reactSuccess().mapToResult().complete();
+        event.jda.shutdown()
+        while (event.jda.status != JDA.Status.SHUTDOWN) {
+            Thread.sleep(15)
+            Thread.onSpinWait()
+        }
 
-		event.getJDA().shutdown();
-
-		while (event.getJDA().getStatus() != JDA.Status.SHUTDOWN) {
-			Thread.sleep(15);
-
-			Thread.onSpinWait();
-		}
-
-		System.exit(0);
-	}
+        exitProcess(0)
+    }
 }
