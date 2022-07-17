71e02d1e6edb851846ac5a5b237d46a09bf1323d
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-17 21:22:39 +0200

    ZIP utils

--- src/main/kotlin/com/freya02/bot/versioning/VersionsUtils.kt
+++ src/main/kotlin/com/freya02/bot/versioning/VersionsUtils.kt
@@ -45,12 +45,12 @@ object VersionsUtils {
         return downloadJavadoc(this.toJitpackUrl(FileType.JAVADOC))
     }
 
-    fun ArtifactInfo.downloadMavenSources(): Path {
-        return downloadJavadoc(this.toMavenUrl(FileType.SOURCES))
+    fun ArtifactInfo.downloadMavenClasses(): Path {
+        return downloadJavadoc(this.toMavenUrl(FileType.CLASSES))
     }
 
-    fun ArtifactInfo.downloadJitpackSources(): Path {
-        return downloadJavadoc(this.toJitpackUrl(FileType.SOURCES))
+    fun ArtifactInfo.downloadJitpackClasses(): Path {
+        return downloadJavadoc(this.toJitpackUrl(FileType.CLASSES))
     }
 
     private fun ArtifactInfo.downloadJavadoc(url: String): Path {
