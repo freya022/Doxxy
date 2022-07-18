800c8ea282257d7f375756a1f444ed6212144ae8
freya02 <41875020+freya022@users.noreply.github.com>
2022-07-18 15:17:06 +0200

    Add note

--- sql/CreateDatabase.sql
+++ sql/CreateDatabase.sql
@@ -17,7 +17,7 @@ create table Doc
     source_id   int  not null,
     type        int  not null,
     className   text not null,
-    identifier  text,
+    identifier  text, --TODO see if we can have a separate column for identifier-without-args as to take advantage of GiST
     embed       text not null,
     source_link text,
 
