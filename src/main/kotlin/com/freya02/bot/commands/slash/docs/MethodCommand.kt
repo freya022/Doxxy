a4107c376aff76bd055680c8b24f90647844c1d8
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-15 21:49:21 +0200

    Refactor MethodCommand

--- src/main/kotlin/com/freya02/bot/commands/slash/docs/MethodCommand.kt
+++ src/main/kotlin/com/freya02/bot/commands/slash/docs/MethodCommand.kt
@@ -3,84 +3,51 @@ package com.freya02.bot.commands.slash.docs
 import com.freya02.bot.docs.DocIndexMap
 import com.freya02.bot.docs.index.DocSuggestion.Companion.mapToSuggestions
 import com.freya02.botcommands.api.annotations.CommandMarker
-import com.freya02.botcommands.api.commands.annotations.GeneratedOption
-import com.freya02.botcommands.api.commands.application.annotations.AppOption
+import com.freya02.botcommands.api.commands.application.GlobalApplicationCommandManager
+import com.freya02.botcommands.api.commands.application.annotations.AppDeclaration
 import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
-import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
 import com.freya02.botcommands.api.components.Components
 import com.freya02.docs.DocSourceType
 
 @CommandMarker
 class MethodCommand(private val docIndexMap: DocIndexMap, private val components: Components) : BaseDocCommand() {
-    @JDASlashCommand(
-        name = "method",
-        subcommand = "botcommands",
-        description = "Shows the documentation for a method of a class"
-    )
-    fun onSlashMethodBC(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = "Name of the Java class",
-            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME
-        ) className: String,
-        @AppOption(
-            description = "Signature of the method",
-            autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
-        ) methodId: String
-    ) {
-        onSlashMethod(event, sourceType, className, methodId)
+    @AppDeclaration
+    fun declare(manager: GlobalApplicationCommandManager) {
+        manager.slashCommand("method") {
+            description = "Shows the documentation for a method of a class"
+
+            DocSourceType.values().forEach { sourceType ->
+                subcommand(sourceType.cmdName) {
+                    description = "Shows the documentation for a method of a class"
+
+                    generatedOption("sourceType") { sourceType }
+
+                    option("className") {
+                        description = "Name of the Java class"
+                        autocompleteReference(CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME)
+                    }
+
+                    option("methodSignature") {
+                        description = "Signature of the method"
+                        autocompleteReference(CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME)
+                    }
+
+                    function = ::onSlashMethod
+                }
+            }
+        }
     }
 
-    @JDASlashCommand(
-        name = "method",
-        subcommand = "jda",
-        description = "Shows the documentation for a method of a class"
-    )
-    fun onSlashMethodJDA(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = "Name of the Java class",
-            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME
-        ) className: String,
-        @AppOption(
-            description = "Signature of the method",
-            autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
-        ) methodId: String
-    ) {
-        onSlashMethod(event, sourceType, className, methodId)
-    }
-
-    @JDASlashCommand(
-        name = "method",
-        subcommand = "java",
-        description = "Shows the documentation for a method of a class"
-    )
-    fun onSlashMethodJava(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = "Name of the Java class",
-            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_METHODS_AUTOCOMPLETE_NAME
-        ) className: String,
-        @AppOption(
-            description = "Signature of the method",
-            autocomplete = CommonDocsHandlers.METHOD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
-        ) methodId: String
-    ) {
-        onSlashMethod(event, sourceType, className, methodId)
-    }
-
-    private fun onSlashMethod(
+    @CommandMarker
+    fun onSlashMethod(
         event: GuildSlashEvent,
         sourceType: DocSourceType,
         className: String,
-        methodId: String
+        methodSignature: String
     ) {
         val docIndex = docIndexMap[sourceType]!!
-        CommonDocsHandlers.handleMethodDocs(event, className, methodId, docIndex, components) {
-            return@handleMethodDocs methodNameByClassAutocomplete(docIndex, className, methodId, 100).mapToSuggestions(className)
+        CommonDocsHandlers.handleMethodDocs(event, className, methodSignature, docIndex, components) {
+            return@handleMethodDocs methodNameByClassAutocomplete(docIndex, className, methodSignature, 100).mapToSuggestions(className)
         }
     }
 }
