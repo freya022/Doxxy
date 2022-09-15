c47c011f949e5cdf8969917f16b1aa3943bfdd90
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-15 21:49:27 +0200

    Refactor SlashResolve

--- src/main/kotlin/com/freya02/bot/commands/slash/docs/SlashResolve.kt
+++ src/main/kotlin/com/freya02/bot/commands/slash/docs/SlashResolve.kt
@@ -6,10 +6,9 @@ import com.freya02.bot.docs.cached.CachedClass
 import com.freya02.bot.docs.cached.CachedField
 import com.freya02.bot.docs.cached.CachedMethod
 import com.freya02.botcommands.api.annotations.CommandMarker
-import com.freya02.botcommands.api.commands.annotations.GeneratedOption
-import com.freya02.botcommands.api.commands.application.annotations.AppOption
+import com.freya02.botcommands.api.commands.application.GlobalApplicationCommandManager
+import com.freya02.botcommands.api.commands.application.annotations.AppDeclaration
 import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
-import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
 import com.freya02.botcommands.api.components.Components
 import com.freya02.docs.DocSourceType
 
@@ -18,55 +17,30 @@ private const val chainArgDescription = "Chain of method/field calls, can also j
 
 @CommandMarker
 class SlashResolve(private val docIndexMap: DocIndexMap, private val components: Components) : BaseDocCommand() {
-    @JDASlashCommand(
-        name = "resolve",
-        subcommand = "botcommands",
-        description = commandDescription
-    )
-    fun onSlashResolveBC(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = chainArgDescription,
-            autocomplete = CommonDocsHandlers.RESOLVE_AUTOCOMPLETE_NAME
-        ) chain: String
-    ) {
-        onSlashResolve(event, sourceType, chain)
+    @AppDeclaration
+    fun declare(manager: GlobalApplicationCommandManager) {
+        manager.slashCommand("resolve") {
+            description = commandDescription
+
+            DocSourceType.values().forEach { sourceType ->
+                subcommand(sourceType.cmdName) {
+                    description = commandDescription
+
+                    generatedOption("sourceType") { sourceType }
+
+                    option("chain") {
+                        description = chainArgDescription
+                        autocompleteReference(CommonDocsHandlers.RESOLVE_AUTOCOMPLETE_NAME)
+                    }
+
+                    function = ::onSlashResolve
+                }
+            }
+        }
     }
 
-    @JDASlashCommand(
-        name = "resolve",
-        subcommand = "jda",
-        description = commandDescription
-    )
-    fun onSlashResolveJDA(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = chainArgDescription,
-            autocomplete = CommonDocsHandlers.RESOLVE_AUTOCOMPLETE_NAME
-        ) chain: String
-    ) {
-        onSlashResolve(event, sourceType, chain)
-    }
-
-    @JDASlashCommand(
-        name = "resolve",
-        subcommand = "java",
-        description = commandDescription
-    )
-    fun onSlashResolveJava(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = chainArgDescription,
-            autocomplete = CommonDocsHandlers.RESOLVE_AUTOCOMPLETE_NAME
-        ) chain: String
-    ) {
-        onSlashResolve(event, sourceType, chain)
-    }
-
-    private fun onSlashResolve(
+    @CommandMarker
+    fun onSlashResolve(
         event: GuildSlashEvent,
         sourceType: DocSourceType,
         chain: String
