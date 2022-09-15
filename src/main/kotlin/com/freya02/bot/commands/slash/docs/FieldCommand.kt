29835cb8bee9f0109719ff2d0f5767556f148d9f
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-15 21:38:33 +0200

    Refactor FieldCommand

--- src/main/kotlin/com/freya02/bot/commands/slash/docs/FieldCommand.kt
+++ src/main/kotlin/com/freya02/bot/commands/slash/docs/FieldCommand.kt
@@ -3,72 +3,43 @@ package com.freya02.bot.commands.slash.docs
 import com.freya02.bot.docs.DocIndexMap
 import com.freya02.bot.docs.index.DocSuggestion.Companion.mapToSuggestions
 import com.freya02.botcommands.api.annotations.CommandMarker
-import com.freya02.botcommands.api.commands.annotations.GeneratedOption
-import com.freya02.botcommands.api.commands.application.annotations.AppOption
+import com.freya02.botcommands.api.commands.application.GlobalApplicationCommandManager
+import com.freya02.botcommands.api.commands.application.annotations.AppDeclaration
 import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
-import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
 import com.freya02.botcommands.api.components.Components
 import com.freya02.docs.DocSourceType
 
 @CommandMarker
 class FieldCommand(private val docIndexMap: DocIndexMap, private val components: Components) : BaseDocCommand() {
-    @JDASlashCommand(
-        name = "field",
-        subcommand = "botcommands",
-        description = "Shows the documentation for a field of a class"
-    )
-    fun onSlashFieldBC(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = "The class to search the field in",
-            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME
-        ) className: String,
-        @AppOption(
-            description = "Name of the field",
-            autocomplete = CommonDocsHandlers.FIELD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
-        ) fieldName: String
-    ) {
-        onSlashField(event, sourceType, className, fieldName)
+    @AppDeclaration
+    fun declare(manager: GlobalApplicationCommandManager) {
+        manager.slashCommand("field") {
+            description = "Shows the documentation for a field of a class"
+
+            DocSourceType.values().forEach { sourceType ->
+                subcommand(sourceType.cmdName) {
+                    description = "Shows the documentation for a field of a class"
+
+                    generatedOption("sourceType") { sourceType }
+
+                    option("className") {
+                        description = "The class to search the field in"
+                        autocompleteReference(CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME)
+                    }
+
+                    option("fieldName") {
+                        description = "Name of the field"
+                        autocompleteReference(CommonDocsHandlers.FIELD_NAME_BY_CLASS_AUTOCOMPLETE_NAME)
+                    }
+
+                    function = ::onSlashField
+                }
+            }
+        }
     }
 
-    @JDASlashCommand(name = "field", subcommand = "jda", description = "Shows the documentation for a field of a class")
-    fun onSlashFieldJDA(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = "The class to search the field in",
-            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME
-        ) className: String,
-        @AppOption(
-            description = "Name of the field",
-            autocomplete = CommonDocsHandlers.FIELD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
-        ) fieldName: String
-    ) {
-        onSlashField(event, sourceType, className, fieldName)
-    }
-
-    @JDASlashCommand(
-        name = "field",
-        subcommand = "java",
-        description = "Shows the documentation for a field of a class"
-    )
-    fun onSlashFieldJava(
-        event: GuildSlashEvent,
-        @GeneratedOption sourceType: DocSourceType,
-        @AppOption(
-            description = "The class to search the field in",
-            autocomplete = CommonDocsHandlers.CLASS_NAME_WITH_FIELDS_AUTOCOMPLETE_NAME
-        ) className: String,
-        @AppOption(
-            description = "Name of the field",
-            autocomplete = CommonDocsHandlers.FIELD_NAME_BY_CLASS_AUTOCOMPLETE_NAME
-        ) fieldName: String
-    ) {
-        onSlashField(event, sourceType, className, fieldName)
-    }
-
-    private fun onSlashField(
+    @CommandMarker
+    fun onSlashField(
         event: GuildSlashEvent,
         sourceType: DocSourceType,
         className: String,
