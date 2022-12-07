e0b1e866272998391b83f1f2bda6eddd0371b0d4
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-07 18:59:28 +0100

    Convert valid package test & disable maven check tests

--- src/test/java/com/freya02/bot/DocSourceTypeValidPackagesTest.kt
+++ src/test/java/com/freya02/bot/DocSourceTypeValidPackagesTest.kt
@@ -1,67 +1,62 @@
-package com.freya02.bot;
+package com.freya02.bot
 
-import com.freya02.docs.DocSourceType;
-import org.junit.jupiter.api.Test;
+import com.freya02.docs.DocSourceType
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+class DocSourceTypeValidPackagesTest {
+    @Test
+    fun testJDA() {
+        Assertions.assertTrue(DocSourceType.JDA.isValidPackage("net.dv8tion.jda"))
+        Assertions.assertFalse(DocSourceType.JDA.isValidPackage("java.lang"))
+    }
 
-public class DocSourceTypeValidPackagesTest {
-	@Test
-	public void testJDA() {
-		assertTrue(DocSourceType.JDA.isValidPackage("net.dv8tion.jda"));
-		assertFalse(DocSourceType.JDA.isValidPackage("java.lang"));
-	}
+    @Test
+    fun testBotCommands() {
+        Assertions.assertTrue(DocSourceType.BOT_COMMANDS.isValidPackage("com.freya02.botcommands.api"))
+        Assertions.assertFalse(DocSourceType.BOT_COMMANDS.isValidPackage("java.lang"))
+    }
 
-	@Test
-	public void testBotCommands() {
-		assertTrue(DocSourceType.BOT_COMMANDS.isValidPackage("com.freya02.botcommands.api"));
-		assertFalse(DocSourceType.BOT_COMMANDS.isValidPackage("java.lang"));
-	}
-
-	@Test
-	public void testJava() {
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.lang"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("com.freya02.botcommands.api"));
-
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.io"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.lang"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.lang.annotation"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.lang.invoke"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.lang.reflect"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.math"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.nio"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.nio.file"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.sql"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.time"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.concurrent"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.concurrent.atomic"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.concurrent.locks"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.function"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.random"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.regex"));
-		assertTrue(DocSourceType.JAVA.isValidPackage("java.util.stream"));
-
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.constant"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.instrument"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.management"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.module"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.ref"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.runtime"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.net"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.channels.spi"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.charsets"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.charsets.spi"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.file.attribute"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.file.spi"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.rmi"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.text"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.util.jar"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.util.logging"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.util.spi"));
-		assertFalse(DocSourceType.JAVA.isValidPackage("java.util.zip"));
-
-		assertFalse(DocSourceType.JAVA.isValidPackage("javaxutil.stream"));
-	}
+    @Test
+    fun testJava() {
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.lang"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("com.freya02.botcommands.api"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.io"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.lang"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.lang.annotation"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.lang.invoke"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.lang.reflect"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.math"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.nio"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.nio.file"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.sql"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.time"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.concurrent"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.concurrent.atomic"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.concurrent.locks"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.function"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.random"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.regex"))
+        Assertions.assertTrue(DocSourceType.JAVA.isValidPackage("java.util.stream"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.constant"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.instrument"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.management"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.module"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.ref"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.lang.runtime"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.net"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.channels.spi"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.charsets"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.charsets.spi"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.file.attribute"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.nio.file.spi"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.rmi"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.text"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.util.jar"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.util.logging"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.util.spi"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("java.util.zip"))
+        Assertions.assertFalse(DocSourceType.JAVA.isValidPackage("javaxutil.stream"))
+    }
 }
