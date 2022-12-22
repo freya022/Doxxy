28eed5857035dc41532e8d00ec1905676d185ec2
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-22 16:47:54 +0100

    Add more stuff in SlashInfo

--- src/main/kotlin/com/freya02/bot/commands/slash/SlashInfo.kt
+++ src/main/kotlin/com/freya02/bot/commands/slash/SlashInfo.kt
@@ -1,46 +1,88 @@
 package com.freya02.bot.commands.slash
 
+import com.freya02.botcommands.api.BCInfo
 import com.freya02.botcommands.api.annotations.CommandMarker
 import com.freya02.botcommands.api.commands.application.ApplicationCommand
 import com.freya02.botcommands.api.commands.application.CommandScope
 import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
 import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
-import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.coroutines.await
+import dev.minn.jda.ktx.interactions.components.row
+import dev.minn.jda.ktx.messages.MessageCreate
 import net.dv8tion.jda.api.JDAInfo
+import net.dv8tion.jda.api.interactions.components.buttons.Button
+import net.dv8tion.jda.api.utils.TimeFormat
+import java.lang.management.ManagementFactory
 
 @CommandMarker
 class SlashInfo : ApplicationCommand() {
     @JDASlashCommand(scope = CommandScope.GLOBAL_NO_DM, name = "info", description = "Gives info on the bot")
-    fun onSlashInfo(event: GuildSlashEvent) {
-        event.replyEmbeds(Embed {
-            author {
-                name = event.jda.selfUser.name
-                iconUrl = event.jda.selfUser.effectiveAvatarUrl
-            }
+    suspend fun onSlashInfo(event: GuildSlashEvent) {
+        event.deferReply(true).queue()
 
-            field {
-                name = "JDA version"
-
-                @Suppress("SENSELESS_COMPARISON") //Might be null depending on version used
-                value = when {
-                    JDAInfo.COMMIT_HASH != null -> "[${JDAInfo.VERSION}](https://github.com/DV8FromTheWorld/JDA/commit/${JDAInfo.COMMIT_HASH})"
-                    else -> "[${JDAInfo.VERSION}](https://github.com/DV8FromTheWorld/JDA)"
+        event.hook.sendMessage(MessageCreate {
+            embed {
+                author {
+                    name = event.jda.selfUser.name
+                    iconUrl = event.jda.selfUser.effectiveAvatarUrl
                 }
 
-                inline = true
+                field {
+                    name = "BotCommands version"
+
+                    @Suppress("SENSELESS_COMPARISON") //Might be null depending on version used
+                    value = when {
+                        BCInfo.COMMIT_HASH != null -> "[${BCInfo.VERSION}](https://github.com/freya022/BotCommands/commit/${BCInfo.COMMIT_HASH})"
+                        else -> "[${BCInfo.VERSION}](https://github.com/freya022/BotCommands)"
+                    }
+
+                    inline = false
+                }
+
+                field {
+                    name = "JDA version"
+
+                    @Suppress("SENSELESS_COMPARISON") //Might be null depending on version used
+                    value = when {
+                        JDAInfo.COMMIT_HASH != null -> "[${JDAInfo.VERSION}](https://github.com/DV8FromTheWorld/JDA/commit/${JDAInfo.COMMIT_HASH})"
+                        else -> "[${JDAInfo.VERSION}](https://github.com/DV8FromTheWorld/JDA)"
+                    }
+
+                    inline = true
+                }
+
+                field {
+                    name = "Gateway version"
+                    value = JDAInfo.DISCORD_GATEWAY_VERSION.toString()
+                    inline = true
+                }
+
+                field {
+                    name = "REST version"
+                    value = JDAInfo.DISCORD_REST_VERSION.toString()
+                    inline = true
+                }
+
+                field {
+                    name = "Uptime"
+                    value = TimeFormat.RELATIVE.format(ManagementFactory.getRuntimeMXBean().startTime)
+                    inline = true
+                }
+
+                field {
+                    name = "REST Ping"
+                    value = "${event.jda.restPing.await()} ms"
+                    inline = true
+                }
+
+                field {
+                    name = "Gateway Ping"
+                    value = "${event.jda.gatewayPing} ms"
+                    inline = true
+                }
             }
 
-            field {
-                name = "Gateway version"
-                value = JDAInfo.DISCORD_GATEWAY_VERSION.toString()
-                inline = true
-            }
-
-            field {
-                name = "REST version"
-                value = JDAInfo.DISCORD_REST_VERSION.toString()
-                inline = true
-            }
-        }).setEphemeral(true).queue()
+            components += row(Button.link("https://github.com/freya022/Doxxy", "Source"))
+        }).queue()
     }
 }
