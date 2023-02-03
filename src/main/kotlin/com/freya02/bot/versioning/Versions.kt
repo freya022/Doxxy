512e91d62be9f2e0ed760b946b637a15905ea6a4
freya022 <41875020+freya022@users.noreply.github.com>
2023-02-03 11:32:03 +0100

    Launch version checks on coroutines Add name to thread Catch exceptions in scheduler

--- src/main/kotlin/com/freya02/bot/versioning/Versions.kt
+++ src/main/kotlin/com/freya02/bot/versioning/Versions.kt
@@ -17,10 +17,15 @@ import com.freya02.botcommands.api.core.annotations.BEventListener
 import com.freya02.botcommands.api.core.annotations.BService
 import com.freya02.botcommands.api.core.events.FirstReadyEvent
 import com.freya02.docs.DocSourceType
-import kotlinx.coroutines.runBlocking
+import dev.minn.jda.ktx.events.getDefaultScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
 
 @BService
 class Versions(private val docIndexMap: DocIndexMap) {
@@ -45,13 +50,16 @@ class Versions(private val docIndexMap: DocIndexMap) {
 
     @BEventListener(async = true, timeout = -1)
     suspend fun initUpdateLoop(event: FirstReadyEvent, context: BContext) {
-        val scheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
+        val scope = getDefaultScope(
+            pool = Executors.newSingleThreadScheduledExecutor { Thread(it, "Version check coroutine") },
+            context = CoroutineName("Version check coroutine")
+        )
 
-        scheduledExecutorService.scheduleWithFixedDelay({ checkLatestBCVersion(context) }, 0, 30, TimeUnit.MINUTES)
-        scheduledExecutorService.scheduleWithFixedDelay({ checkLatestJDAVersionFromBC() }, 0, 30, TimeUnit.MINUTES)
-        scheduledExecutorService.scheduleWithFixedDelay({ checkLatestJDAVersion(context) }, 0, 30, TimeUnit.MINUTES)
-        scheduledExecutorService.scheduleWithFixedDelay({ checkLatestJDAKtxVersion() }, 0, 30, TimeUnit.MINUTES)
-        scheduledExecutorService.scheduleWithFixedDelay({ checkLatestLavaPlayerVersion() }, 0, 30, TimeUnit.MINUTES)
+        scope.scheduleWithFixedDelay(30.minutes, "BC") { checkLatestBCVersion(context) }
+        scope.scheduleWithFixedDelay(30.minutes, "JDA from BC") { checkLatestJDAVersionFromBC() }
+        scope.scheduleWithFixedDelay(30.minutes, "JDA") { checkLatestJDAVersion(context) }
+        scope.scheduleWithFixedDelay(30.minutes, "JDA-KTX") { checkLatestJDAKtxVersion() }
+        scope.scheduleWithFixedDelay(30.minutes, "LavaPlayer") { checkLatestLavaPlayerVersion() }
 
         //First index for Java's docs, may take some time
         if (docIndexMap[DocSourceType.JAVA]!!.getClassDoc("Object") == null) {
@@ -64,119 +72,103 @@ class Versions(private val docIndexMap: DocIndexMap) {
         }
     }
 
-    private fun checkLatestJDAVersion(context: BContext?): Boolean {
-        try {
-            val changed = jdaChecker.checkVersion()
+    private inline fun CoroutineScope.scheduleWithFixedDelay(duration: Duration, desc: String, crossinline block: suspend () -> Unit) {
+        launch {
+            try {
+                block()
+            } catch (e: Throwable) {
+                logger.error("An error occurred while checking latest $desc version", e)
+            }
+            delay(duration)
+        }
+    }
 
-            if (changed) {
-                logger.info("JDA version changed")
+    private suspend fun checkLatestJDAVersion(context: BContext) {
+        val changed = jdaChecker.checkVersion()
 
-                logger.trace("Downloading JDA javadocs")
-                jdaChecker.latest.downloadMavenJavadoc().withTemporaryFile { tempZip ->
-                    logger.trace("Extracting JDA javadocs")
-                    VersionsUtils.replaceWithZipContent(tempZip, jdaDocsFolder, "html")
-                }
+        if (changed) {
+            logger.info("JDA version changed")
 
-                jdaChecker.latest.downloadMavenSources().withTemporaryFile { tempZip ->
-                    logger.trace("Extracting JDA sources")
-                    VersionsUtils.extractZip(tempZip, jdaDocsFolder, "java")
-                }
-
-                val sourceUrl = GithubUtils.getLatestReleaseHash("DV8FromTheWorld", "JDA")
-                    ?.let { hash -> "https://github.com/DV8FromTheWorld/JDA/blob/${hash.hash}/src/main/java/" }
-
-                logger.trace("Invalidating JDA index")
-                runBlocking { docIndexMap.refreshAndInvalidateIndex(DocSourceType.JDA, ReindexData(sourceUrl)) }
-                for (handlerName in CommonDocsHandlers.AUTOCOMPLETE_NAMES) {
-                    context?.invalidateAutocompleteCache(handlerName)
-                }
-
-                jdaChecker.saveVersion()
-
-                logger.info("JDA version updated to {}", jdaChecker.latest.version)
+            logger.trace("Downloading JDA javadocs")
+            jdaChecker.latest.downloadMavenJavadoc().withTemporaryFile { tempZip ->
+                logger.trace("Extracting JDA javadocs")
+                VersionsUtils.replaceWithZipContent(tempZip, jdaDocsFolder, "html")
             }
 
-            return changed
-        } catch (t: Throwable) {
-            logger.error("An exception occurred while retrieving versions", t)
-        }
+            jdaChecker.latest.downloadMavenSources().withTemporaryFile { tempZip ->
+                logger.trace("Extracting JDA sources")
+                VersionsUtils.extractZip(tempZip, jdaDocsFolder, "java")
+            }
 
-        return false
+            val sourceUrl = GithubUtils.getLatestReleaseHash("DV8FromTheWorld", "JDA")
+                ?.let { hash -> "https://github.com/DV8FromTheWorld/JDA/blob/${hash.hash}/src/main/java/" }
+
+            logger.trace("Invalidating JDA index")
+            docIndexMap.refreshAndInvalidateIndex(DocSourceType.JDA, ReindexData(sourceUrl))
+            for (handlerName in CommonDocsHandlers.AUTOCOMPLETE_NAMES) {
+                context.invalidateAutocompleteCache(handlerName)
+            }
+
+            jdaChecker.saveVersion()
+
+            logger.info("JDA version updated to {}", jdaChecker.latest.version)
+        }
     }
 
     private fun checkLatestJDAKtxVersion() {
-        try {
-            val changed = jdaKtxChecker.checkVersion()
-            if (changed) {
-                logger.info("JDA-KTX version changed")
-                jdaKtxChecker.saveVersion()
-                logger.info("JDA-KTX version updated to {}", jdaKtxChecker.latest.version)
-            }
-        } catch (t: Throwable) {
-            logger.error("An exception occurred while retrieving versions", t)
+        val changed = jdaKtxChecker.checkVersion()
+        if (changed) {
+            logger.info("JDA-KTX version changed")
+            jdaKtxChecker.saveVersion()
+            logger.info("JDA-KTX version updated to {}", jdaKtxChecker.latest.version)
         }
     }
 
     private fun checkLatestLavaPlayerVersion() {
-        try {
-            val changed = lavaPlayerChecker.checkVersion()
-            if (changed) {
-                logger.info("LavaPlayer version changed")
-                lavaPlayerChecker.saveVersion()
-                logger.info("LavaPlayer version updated to {}", lavaPlayerChecker.latest.version)
-            }
-        } catch (t: Throwable) {
-            logger.error("An exception occurred while retrieving versions", t)
+        val changed = lavaPlayerChecker.checkVersion()
+        if (changed) {
+            logger.info("LavaPlayer version changed")
+            lavaPlayerChecker.saveVersion()
+            logger.info("LavaPlayer version updated to {}", lavaPlayerChecker.latest.version)
         }
     }
 
+    //TODO use coroutines in version checkers / HTTP
     private fun checkLatestJDAVersionFromBC() {
-        try {
-            val changed = jdaVersionFromBCChecker.checkVersion()
-            if (changed) {
-                logger.info("BotCommands's JDA version changed")
-                jdaVersionFromBCChecker.saveVersion()
-                logger.info("BotCommands's JDA version updated to {}", jdaVersionFromBCChecker.latest.version)
-            }
-        } catch (t: Throwable) {
-            logger.error("An exception occurred while retrieving versions", t)
+        val changed = jdaVersionFromBCChecker.checkVersion()
+        if (changed) {
+            logger.info("BotCommands's JDA version changed")
+            jdaVersionFromBCChecker.saveVersion()
+            logger.info("BotCommands's JDA version updated to {}", jdaVersionFromBCChecker.latest.version)
         }
     }
 
-    fun checkLatestBCVersion(context: BContext?): Boolean {
-        try {
-            val changed = bcChecker.checkVersion()
+    private suspend fun checkLatestBCVersion(context: BContext) {
+        val changed = bcChecker.checkVersion()
 
-            if (changed) {
-                logger.info("BotCommands version changed")
+        if (changed) {
+            logger.info("BotCommands version changed")
 
-                bcChecker.latest.downloadMavenJavadoc().withTemporaryFile { javadocPath ->
-                    logger.trace("Extracting BC javadocs")
-                    VersionsUtils.replaceWithZipContent(javadocPath, bcDocsFolder, "html")
-                }
-
-                bcChecker.latest.downloadMavenSources().withTemporaryFile { javadocPath ->
-                    logger.trace("Extracting BC sources")
-                    VersionsUtils.extractZip(javadocPath, bcDocsFolder, "java")
-                }
-
-                logger.trace("Invalidating BotCommands index")
-                runBlocking { docIndexMap.refreshAndInvalidateIndex(DocSourceType.BOT_COMMANDS, ReindexData()) }
-                for (handlerName in CommonDocsHandlers.AUTOCOMPLETE_NAMES) {
-                    context?.invalidateAutocompleteCache(handlerName)
-                }
-
-                bcChecker.saveVersion()
-
-                logger.info("BotCommands version updated to {}", bcChecker.latest.version)
+            bcChecker.latest.downloadMavenJavadoc().withTemporaryFile { javadocPath ->
+                logger.trace("Extracting BC javadocs")
+                VersionsUtils.replaceWithZipContent(javadocPath, bcDocsFolder, "html")
             }
 
-            return changed
-        } catch (t: Throwable) {
-            logger.error("An exception occurred while retrieving versions", t)
-        }
+            bcChecker.latest.downloadMavenSources().withTemporaryFile { javadocPath ->
+                logger.trace("Extracting BC sources")
+                VersionsUtils.extractZip(javadocPath, bcDocsFolder, "java")
+            }
 
-        return false
+            logger.trace("Invalidating BotCommands index")
+            docIndexMap.refreshAndInvalidateIndex(DocSourceType.BOT_COMMANDS, ReindexData())
+            for (handlerName in CommonDocsHandlers.AUTOCOMPLETE_NAMES) {
+                context.invalidateAutocompleteCache(handlerName)
+            }
+
+            bcChecker.saveVersion()
+
+            logger.info("BotCommands version updated to {}", bcChecker.latest.version)
+        }
     }
 
     val latestBotCommandsVersion: ArtifactInfo
