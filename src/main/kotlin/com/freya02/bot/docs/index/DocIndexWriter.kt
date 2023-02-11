bd647ed962a813e384f288b7294f8632c126ac0f
freya02 <41875020+freya022@users.noreply.github.com>
2023-02-11 21:57:36 +0100

    Refactor DocUtils#getSimpleAnnotatedSignature

--- src/main/kotlin/com/freya02/bot/docs/index/DocIndexWriter.kt
+++ src/main/kotlin/com/freya02/bot/docs/index/DocIndexWriter.kt
@@ -7,7 +7,7 @@ import com.freya02.botcommands.api.core.db.Database
 import com.freya02.botcommands.api.core.db.Transaction
 import com.freya02.docs.ClassDocs
 import com.freya02.docs.DocSourceType
-import com.freya02.docs.DocUtils.getReturnTypeNoAnnotations
+import com.freya02.docs.DocUtils.returnTypeNoAnnotations
 import com.freya02.docs.DocsSession
 import com.freya02.docs.data.BaseDoc
 import com.freya02.docs.data.ClassDetailType
@@ -193,7 +193,7 @@ internal class DocIndexWriter(
                 baseDoc.identifierNoArgs,
                 baseDoc.humanIdentifier,
                 baseDoc.toHumanClassIdentifier(className),
-                baseDoc.getReturnTypeNoAnnotations(),
+                baseDoc.returnTypeNoAnnotations,
                 embedJson,
                 baseDoc.onlineURL,
                 sourceLink
