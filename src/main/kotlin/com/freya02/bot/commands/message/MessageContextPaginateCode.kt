d689bddbcf36573ea72a4d651dc5f82bb9f55ef2
freya02 <41875020+freya022@users.noreply.github.com>
2023-02-12 21:18:41 +0100

    Refactor into a state machine Add "Show line numbers" button

--- src/main/kotlin/com/freya02/bot/commands/message/MessageContextPaginateCode.kt
+++ src/main/kotlin/com/freya02/bot/commands/message/MessageContextPaginateCode.kt
@@ -1,66 +1,139 @@
 package com.freya02.bot.commands.message
 
+import com.freya02.bot.pagination.CodePaginator
 import com.freya02.bot.pagination.CodePaginatorBuilder
 import com.freya02.bot.utils.suppressContentWarning
 import com.freya02.botcommands.api.annotations.CommandMarker
 import com.freya02.botcommands.api.commands.application.ApplicationCommand
 import com.freya02.botcommands.api.commands.application.context.annotations.JDAMessageCommand
 import com.freya02.botcommands.api.commands.application.context.message.GuildMessageEvent
+import com.freya02.botcommands.api.components.Button
 import com.freya02.botcommands.api.components.Components
 import com.freya02.botcommands.api.components.data.InteractionConstraints
+import com.freya02.botcommands.api.pagination.PaginatorComponents
 import dev.minn.jda.ktx.coroutines.await
+import dev.minn.jda.ktx.interactions.components.asDisabled
+import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.reply_
 import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.utils.messages.MessageCreateData
+import net.dv8tion.jda.api.entities.UserSnowflake
+import net.dv8tion.jda.api.interactions.InteractionHook
+import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
+import net.dv8tion.jda.api.utils.messages.MessageEditBuilder
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
+
+private typealias MessageId = Long
 
 @CommandMarker
 class MessageContextPaginateCode(private val componentsService: Components) : ApplicationCommand() {
-    private val codeBlockLength = "```java\n```".length
+    private class PaginationState(val paginator: CodePaginator, val originalContent: String, val owner: UserSnowflake) {
+        var showLineNumbers: Boolean by Delegates.observable(false) { _, _, _ -> blocks = regenerateBlocks() }
+        var replaceStrings: Boolean by Delegates.observable(false) { _, _, _ -> blocks = regenerateBlocks() }
+        var useFormatting: Boolean by Delegates.observable(false) { _, _, _ -> blocks = regenerateBlocks() }
+
+        var blocks: List<String> = regenerateBlocks()
+            private set
+
+        private fun regenerateBlocks() = buildList {
+            val builder = StringBuilder()
+
+            originalContent.lines()
+                .forEachIndexed { index, line ->
+                    val toBeAppended = buildString(line.length + 10) {
+                        if (showLineNumbers) append("${index + 1}  ")
+                        appendLine(line)
+                    }
+
+                    if (builder.length + toBeAppended.length + codeBlockLength > Message.MAX_CONTENT_LENGTH) {
+                        add(builder.toString())
+                        builder.clear()
+                    }
+
+                    builder.append(toBeAppended)
+                }.also { add(builder.toString()) }
+        }.also {
+            paginator.maxPages = it.size
+            paginator.page = 0
+        }
+
+        private companion object {
+            private const val codeBlockLength = "```java\n```".length
+        }
+    }
+
+    private val emptyEmbed = Embed { description = "dummy" }
+    private val codeMap: MutableMap<MessageId, PaginationState> = hashMapOf()
 
     @JDAMessageCommand(name = "Paginate code")
     suspend fun onMessageContextPaginateCode(event: GuildMessageEvent) {
         event.deferReply(true).queue()
 
+        withCodeContent(event, event.target) { content ->
+            val hook = event.hook
+            val messageId = event.target.idLong
+            val paginator = CodePaginatorBuilder(componentsService)
+                .setConstraints(InteractionConstraints.ofUsers(event.user))
+                .setTimeout(10, TimeUnit.MINUTES) { instance, _ ->
+                    codeMap.remove(messageId) //Always executed at some point as there is no delete button
+                    hook.editOriginalComponents().queue()
+                    instance.cleanup()
+                }
+                .setPaginatorSupplier { _, editBuilder, components, page ->
+                    emptyEmbed.also { onPageChange(messageId, editBuilder, components, page) }
+                }
+                .build()
+
+            val paginationState = PaginationState(paginator, content, event.user)
+            codeMap[messageId] = paginationState
+
+            sendCodePaginator(event.hook, paginationState)
+        }
+    }
+
+    private fun onPageChange(
+        messageId: Long,
+        editBuilder: MessageEditBuilder,
+        components: PaginatorComponents,
+        page: Int
+    ) {
+        val state = codeMap[messageId]!!
+        val blocks = state.blocks
+
+        val lineNumbersButton = makeLineNumbersButton(state)
+        components.addComponents(lineNumbersButton)
+
+        editBuilder.setContent("```java\n${blocks[page]}```")
+    }
+
+    private fun sendCodePaginator(hook: InteractionHook, state: PaginationState) {
+        val paginator = state.paginator
+        hook.editOriginal(paginator.get()).queue()
+    }
+
+    private fun makeLineNumbersButton(state: PaginationState): Button {
+        val prefix = if (state.showLineNumbers) "Hide" else "Show"
+        return componentsService.ephemeralButton(ButtonStyle.SECONDARY, "$prefix line numbers") {
+            constraints += state.owner
+            bindTo { buttonEvent ->
+                buttonEvent.editComponents(buttonEvent.message.components.asDisabled()).queue()
+                state.showLineNumbers = !state.showLineNumbers
+                sendCodePaginator(buttonEvent.hook, state)
+            }
+        }
+    }
+
+    private suspend fun withCodeContent(event: IReplyCallback, message: Message, block: suspend (String) -> Unit) {
         val attachment = suppressContentWarning {
-            if (event.target.attachments.size != 1) {
+            if (message.attachments.size != 1) {
                 return event.reply_("There is must be 1 attachment in this message", ephemeral = true).queue()
             } //TODO support code blocks
 
-            event.target.attachments.single()
+            message.attachments.single()
         }
 
-        val blocks: List<String> = attachment.proxy.download().await()
-            .use { it.readAllBytes().decodeToString() }
-            .let { makeCodeBlocks(it, showLineNumbers = true) }
-
-        CodePaginatorBuilder(componentsService, blocks)
-            .setConstraints(InteractionConstraints.ofUsers(event.user))
-            .setTimeout(10, TimeUnit.MINUTES) { paginator, _ ->
-                event.hook.editOriginalComponents().queue()
-                paginator.cleanup()
-            }
-            .build()
-            .get()
-            .let { event.hook.sendMessage(MessageCreateData.fromEditData(it)).queue() }
-    }
-
-    private fun makeCodeBlocks(content: String, showLineNumbers: Boolean) = buildList {
-        val builder = StringBuilder()
-
-        content.lines()
-            .forEachIndexed { index, line ->
-                val toBeAppended = buildString(line.length + 10) {
-                    if (showLineNumbers) append("${index + 1}  ")
-                    appendLine(line)
-                }
-
-                if (builder.length + toBeAppended.length + codeBlockLength > Message.MAX_CONTENT_LENGTH) {
-                    add(builder.toString())
-                    builder.clear()
-                }
-
-                builder.append(toBeAppended)
-            }.also { add(builder.toString()) }
+        val content = attachment.proxy.download().await().use { it.readAllBytes().decodeToString() }
+        block(content)
     }
 }
--- src/main/kotlin/com/freya02/bot/pagination/CodePagination.kt
+++ src/main/kotlin/com/freya02/bot/pagination/CodePagination.kt
@@ -7,21 +7,17 @@ import com.freya02.botcommands.api.pagination.TimeoutInfo
 import com.freya02.botcommands.api.pagination.paginator.BasicPaginator
 import com.freya02.botcommands.api.pagination.paginator.BasicPaginatorBuilder
 import com.freya02.botcommands.api.utils.ButtonContent
-import dev.minn.jda.ktx.messages.Embed
 
 class CodePaginatorBuilder(
-    componentsService: Components,
-    private val blocks: List<String>
+    componentsService: Components
 ) : BasicPaginatorBuilder<CodePaginatorBuilder, CodePaginator>(componentsService) {
     override fun build() =
         CodePaginator(
             componentsService,
             constraints,
             timeout,
-            _maxPages = blocks.size,
-            supplier = { _, editBuilder, _, page ->
-                emptyEmbed.also { editBuilder.setContent("```java\n${blocks[page]}```") }
-            },
+            _maxPages = 0,
+            paginatorSupplier,
             hasDeleteButton,
             firstContent,
             previousContent,
@@ -29,15 +25,6 @@ class CodePaginatorBuilder(
             lastContent,
             deleteContent
         )
-
-    @Deprecated("Already handled", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith(""))
-    override fun setPaginatorSupplier(paginatorSupplier: PaginatorSupplier<CodePaginator>): CodePaginatorBuilder {
-        return super.setPaginatorSupplier(paginatorSupplier)
-    }
-
-    private companion object {
-        private val emptyEmbed = Embed { description = "dummy" }
-    }
 }
 
 class CodePaginator(
@@ -65,6 +52,10 @@ class CodePaginator(
     lastContent,
     deleteContent
 ) {
+    public override fun setMaxPages(maxPages: Int) {
+        super.setMaxPages(maxPages)
+    }
+
     override fun onPostGet() {
         super.onPostGet()
         messageBuilder.setEmbeds()
