fd8f93b4ebb8bb1f0d919ffaa54b838ebb5a2b11
freya02 <41875020+freya022@users.noreply.github.com>
2023-02-13 20:51:57 +0100

    Use Modal#await in SlashFormat

--- src/main/kotlin/com/freya02/bot/commands/slash/SlashFormat.kt
+++ src/main/kotlin/com/freya02/bot/commands/slash/SlashFormat.kt
@@ -7,16 +7,10 @@ import com.freya02.botcommands.api.commands.application.CommandScope
 import com.freya02.botcommands.api.commands.application.slash.GlobalSlashEvent
 import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
 import com.freya02.botcommands.api.modals.Modals
-import com.freya02.botcommands.api.modals.annotations.ModalHandler
-import com.freya02.botcommands.api.modals.annotations.ModalInput
 import com.freya02.botcommands.api.modals.create
 import com.freya02.botcommands.api.modals.paragraphTextInput
 import dev.minn.jda.ktx.messages.reply_
 import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
-
-private const val FORMAT_MODAL_HANDLER_NAME = "SlashFormat: formatModal"
-private const val FORMAT_MODAL_CODE_INPUT_NAME = "SlashFormat: formatModalCodeInput"
 
 @CommandMarker
 class SlashFormat : ApplicationCommand() {
@@ -25,33 +19,30 @@ class SlashFormat : ApplicationCommand() {
         name = "format",
         description = "Formats your code and sends it back to you as a copy-paste-able block"
     )
-    fun onSlashFormat(event: GlobalSlashEvent, modals: Modals) {
+    suspend fun onSlashFormat(event: GlobalSlashEvent, modals: Modals) {
         val modal = modals.create("Format code") {
-            paragraphTextInput(FORMAT_MODAL_CODE_INPUT_NAME, "Code to format")
-
-            bindTo(FORMAT_MODAL_HANDLER_NAME)
+            paragraphTextInput("code", "Code to format") {
+                id = "code"
+            }
         }
 
         event.replyModal(modal).queue()
-    }
 
-    @ModalHandler(name = FORMAT_MODAL_HANDLER_NAME)
-    fun onFormatModalSubmit(
-        event: ModalInteractionEvent,
-        @ModalInput(name = FORMAT_MODAL_CODE_INPUT_NAME) code: String
-    ) {
-        val formattedSource =
-            Formatter.format(code)?.let {
-                """
-\`\`\`java
-$it
-\`\`\`"""
-            } ?: "Cannot format this code, this formatter only supports Java, your snippet may have a syntax error."
+        val modalEvent = modal.await()
+        val code = modalEvent.getValue("code")!!.asString
+
+        val formattedSource = Formatter.format(code)?.let {
+            """
+                \`\`\`java
+                $it
+                \`\`\`
+            """.trimIndent()
+        } ?: "Cannot format this code, this formatter only supports Java, your snippet may have a syntax error."
 
         if (formattedSource.length > Message.MAX_CONTENT_LENGTH) {
-            event.reply_("The formatted code is too large, please use a paste service.", ephemeral = true).queue()
+            modalEvent.reply_("The formatted code is too large, please use a paste service.", ephemeral = true).queue()
         } else {
-            event.reply_(formattedSource, ephemeral = true).queue()
+            modalEvent.reply_(formattedSource, ephemeral = true).queue()
         }
     }
 }
