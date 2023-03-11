85790d5985fb95b0cce3d52c3692bdb9a0d57016
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-11 19:49:46 +0100

    Add profiler logging utilities

---
+++ src/main/kotlin/com/freya02/bot/utils/LoggerUtils.kt
@@ -0,0 +1,39 @@
+package com.freya02.bot.utils
+
+import mu.KotlinLogging
+import org.slf4j.profiler.Profiler
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+@OptIn(ExperimentalContracts::class)
+inline fun <R> createProfiler(name: String, block: Profiler.() -> R): R {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    return Profiler(name).apply {
+        logger = KotlinLogging.logger { }
+    }.let { profiler ->
+        profiler.block().also { profiler.stop().log() }
+    }
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <R> Profiler.nextStep(name: String, block: Profiler.() -> R): R {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    start(name)
+    return block()
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <R> Profiler.nestedProfiler(name: String, block: Profiler.() -> R): R {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    return startNested(name).block()
+}
