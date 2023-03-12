e4faadf834e7d7deb3a1ad4795e1395f8e28f97d
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-12 17:33:55 +0100

    Add first iteration of ImplementationMetadata

---
+++ src/main/kotlin/com/freya02/bot/docs/metadata/ImplementationMetadata.kt
@@ -0,0 +1,87 @@
+package com.freya02.bot.docs.metadata
+
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration
+import com.github.javaparser.resolution.types.ResolvedReferenceType
+import mu.KotlinLogging
+import java.util.*
+
+private typealias ResolvedClass = ResolvedReferenceType
+private typealias ResolvedMethod = ResolvedMethodDeclaration
+
+class ImplementationMetadata private constructor(compilationUnits: List<CompilationUnit>) {
+    private val resolvedClassComparator: Comparator<ResolvedClass> = Comparator.comparing { it.qualifiedName }
+    private val resolvedMethodComparator: Comparator<ResolvedMethod> = Comparator.comparing { it.qualifiedName + it.fixedDescriptor }
+
+    val subclassesMap: MutableMap<ResolvedClass, MutableList<ResolvedReferenceTypeDeclaration>> =
+        Collections.synchronizedMap(TreeMap(resolvedClassComparator))
+
+    // BaseClass -> Map<TheMethod, List<ClassOverridingMethod>>
+    val classToMethodImplementations: MutableMap<ResolvedClass, MutableMap<ResolvedMethod, MutableList<ResolvedReferenceTypeDeclaration>>> =
+        Collections.synchronizedMap(TreeMap(resolvedClassComparator))
+
+    init {
+        compilationUnits.forEachCompilationUnit(logger, ::processCU)
+    }
+
+    private fun processCU(cu: CompilationUnit) {
+        cu.findAll(ClassOrInterfaceDeclaration::class.java)
+//            .filter { it.nameAsString.contains("RestAction") }
+            .map { it.resolve() }
+            .forEach { resolvedCU ->
+                val ancestors: List<ResolvedClass> = resolvedCU.getAllAncestors(ResolvedReferenceTypeDeclaration.breadthFirstFunc)
+
+                resolvedCU.declaredMethods.forEach { resolvedMethod: ResolvedMethod ->
+                    val overriddenClasses = ancestors.filter { ancestor ->
+                        ancestor.declaredMethods.any { ancestorMethod ->
+                            resolvedMethod.name == ancestorMethod.name
+                                    && resolvedMethod.numberOfParams == ancestorMethod.noParams
+                                    && (0 until ancestorMethod.noParams).all { i ->
+                                ancestorMethod.getParamType(i).isAssignableBy(resolvedMethod.getParam(i).type)
+                            }
+                        }
+                    }
+
+                    overriddenClasses.forEach { overriddenClass ->
+                        classToMethodImplementations
+                            .computeIfAbsent(overriddenClass) { Collections.synchronizedMap(TreeMap(resolvedMethodComparator)) }
+                            .computeIfAbsent(resolvedMethod) { Collections.synchronizedList(arrayListOf()) }
+                            .add(resolvedCU)
+                    }
+                }
+
+                ancestors.forEach {
+                    subclassesMap.computeIfAbsent(it) { Collections.synchronizedList(arrayListOf()) }.add(resolvedCU)
+                }
+            }
+    }
+
+    private val ResolvedMethodDeclaration.fixedDescriptor: String
+        get() = buildString {
+            append('(')
+            for(i in 0..<numberOfParams) {
+                append(getParam(i).type.describe())
+            }
+            append(')')
+
+            append(returnType.describe())
+        }
+
+    companion object {
+        private val logger = KotlinLogging.logger { }
+
+        fun <T> Map<ResolvedClass, T>.findByClassName(name: String): T {
+            return this.toList().first { (k, _) -> k.qualifiedName.endsWith(".$name") }.second
+        }
+
+        fun <T> Map<ResolvedMethod, T>.findByMethodName(name: String): Map<String, T> {
+            return this.filterKeys { it.name == name }.mapKeys { (k, _) -> k.className }
+        }
+
+        fun fromCompilationUnits(compilationUnits: List<CompilationUnit>): ImplementationMetadata {
+            return ImplementationMetadata(compilationUnits)
+        }
+    }
+}
--- src/test/kotlin/com/freya02/docs/ImplResolverTest.kt
+++ src/test/kotlin/com/freya02/docs/ImplResolverTest.kt
@@ -1,28 +1,26 @@
 package com.freya02.docs
 
+import com.freya02.bot.docs.metadata.ImplementationMetadata
 import com.freya02.bot.versioning.VersionsUtils
-import com.github.javaparser.ast.CompilationUnit
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration
-import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration
 import com.github.javaparser.resolution.types.ResolvedReferenceType
 import com.github.javaparser.symbolsolver.JavaSymbolSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver
 import com.github.javaparser.utils.SourceRoot
+import mu.KotlinLogging
 import java.io.FileNotFoundException
 import java.nio.file.Path
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
 import kotlin.io.path.*
-import kotlin.system.measureTimeMillis
 
 
 private typealias ResolvedClass = ResolvedReferenceType
 private typealias ResolvedMethod = ResolvedMethodDeclaration
 
 object ImplResolverTest {
+    private val logger = KotlinLogging.logger { }
+
 //    private val rootPath = Data.javadocsPath.resolve("JDA")
     private val rootPath: Path = run {
         System.getProperty("java.class.path")
@@ -45,73 +43,30 @@ object ImplResolverTest {
                 }
             }
     }
+
     private val sourceRoot = SourceRoot(rootPath)
     private val solver = JavaSymbolSolver(CombinedTypeSolver().also { combinedTypeSolver ->
         combinedTypeSolver.add(JavaParserTypeSolver(rootPath))
         combinedTypeSolver.add(ReflectionTypeSolver(/* jreOnly = */ false))
     })
 
-    private val subclassesMap: MutableMap<ResolvedClass, MutableList<ResolvedReferenceTypeDeclaration>> = ConcurrentHashMap()
-
-    // BaseClass -> Map<TheMethod, List<ClassOverridingMethod>>
-    private val classToMethodImplementations: MutableMap<ResolvedClass, MutableMap<ResolvedMethod, MutableList<ResolvedReferenceTypeDeclaration>>> = ConcurrentHashMap()
-
-    fun <T> Map<ResolvedClass, T>.findByClassName(name: String): T {
-        return this.toList().first { (k, _) -> k.qualifiedName.endsWith(".$name") }.second
-    }
-
-    fun <T> Map<ResolvedMethod, T>.findByMethodName(name: String): Map<String, T> {
-        return this.filterKeys { it.name == name }.mapKeys { (k, _) -> k.className }
-    }
-
     @JvmStatic
     fun main(args: Array<String>) {
-        measureTimeMillis {
-            sourceRoot.parserConfiguration.setSymbolResolver(solver)
-            sourceRoot.parseParallelized("net.dv8tion.jda") { _, _, result ->
-                result.ifSuccessful(::processCU)
-                if (!result.isSuccessful) {
-                    throw RuntimeException(result.problems.toString())
+        sourceRoot.parserConfiguration.setSymbolResolver(solver)
+        sourceRoot
+            .tryToParse("net.dv8tion.jda")
+            .filter { result ->
+                if (result.problems.isNotEmpty()) {
+                    result.problems.forEach { logger.error(it.toString()) }
+                } else if (!result.isSuccessful) {
+                    logger.error("Unexpected failure while parsing CU")
                 }
 
-                return@parseParallelized SourceRoot.Callback.Result.DONT_SAVE
-            }
-        }.also { println("Took $it ms") }
+                result.isSuccessful
+            }.map { it.result.get() }
 
-        println(subclassesMap.size)
-        println(classToMethodImplementations.size)
+        val implementationMetadata = ImplementationMetadata.fromCompilationUnits(sourceRoot.compilationUnits)
 
         println()
-    }
-
-    private fun processCU(cu: CompilationUnit) {
-        cu.findAll(ClassOrInterfaceDeclaration::class.java)
-            .map { it.resolve() }
-            .forEach { resolvedCU ->
-                val ancestors: List<ResolvedClass> = resolvedCU.getAllAncestors(ResolvedReferenceTypeDeclaration.breadthFirstFunc)
-
-                resolvedCU.declaredMethods.forEach { resolvedMethod: ResolvedMethod ->
-                    val overriddenClasses = ancestors.filter { ancestor ->
-                        ancestor.declaredMethods.any { ancestorMethod ->
-                            resolvedMethod.name == ancestorMethod.name
-                                && resolvedMethod.numberOfParams == ancestorMethod.noParams
-                                && (0 until ancestorMethod.noParams).all { i ->
-                                ancestorMethod.getParamType(i).isAssignableBy(resolvedMethod.getParam(i).type)
-                            }
-                        }
-                    }
-
-                    overriddenClasses.forEach { overriddenClass ->
-                        classToMethodImplementations
-                            .computeIfAbsent(overriddenClass) { ConcurrentHashMap() }
-                            .computeIfAbsent(resolvedMethod) { Collections.synchronizedList(arrayListOf()) }
-                            .add(resolvedCU)
-                    }
-                }
-
-                ancestors.forEach {
-                    subclassesMap.computeIfAbsent(it) { Collections.synchronizedList(arrayListOf()) }.add(resolvedCU)
-                }
-            }
     }
 }
