f3bb8dedddb82dd6c3264af1621fb67775a4c32f
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-16 20:54:41 +0100

    Change method implementation parsing, reformat Now computes all subclasses first, then works from the most generic class up to the subclasses to find implementations

--- src/main/kotlin/com/freya02/bot/docs/metadata/ImplementationMetadata.kt
+++ src/main/kotlin/com/freya02/bot/docs/metadata/ImplementationMetadata.kt
@@ -2,10 +2,10 @@ package com.freya02.bot.docs.metadata
 
 import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.resolution.MethodUsage
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration
 import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration
 import com.github.javaparser.resolution.types.ResolvedReferenceType
-import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration
 import mu.KotlinLogging
 import java.util.*
 
@@ -15,69 +15,74 @@ private typealias ResolvedMethod = ResolvedMethodDeclaration
 class ImplementationMetadata private constructor(compilationUnits: List<CompilationUnit>) {
     // Comparators are used to determine equality, as JP instances likely do not implement hashCode/equals correctly
     private val resolvedClassComparator: Comparator<ResolvedClass> = Comparator.comparing { it.qualifiedName }
-    private val resolvedMethodComparator: Comparator<ResolvedMethod> = Comparator.comparing { it.qualifiedName + it.fixedDescriptor }
-    private val resolvedReferenceTypeDeclarationComparator: Comparator<ResolvedReferenceTypeDeclaration> = Comparator.comparing { it.qualifiedName }
+    private val resolvedMethodComparator: Comparator<ResolvedMethod> =
+        Comparator.comparing { it.qualifiedName + it.fixedDescriptor }
+    private val resolvedReferenceTypeDeclarationComparator: Comparator<ResolvedReferenceTypeDeclaration> =
+        Comparator.comparing { it.qualifiedName }
 
     val subclassesMap: MutableMap<ResolvedClass, MutableList<ResolvedReferenceTypeDeclaration>> =
         Collections.synchronizedMap(TreeMap(resolvedClassComparator))
 
     // BaseClass -> Map<TheMethod, Set<ClassOverridingMethod>>
-    val classToMethodImplementations: MutableMap<ResolvedReferenceTypeDeclaration, MutableMap<ResolvedMethod, MutableSet<ResolvedReferenceTypeDeclaration>>> =
-        Collections.synchronizedMap(TreeMap(resolvedReferenceTypeDeclarationComparator))
+    val classToMethodImplementations: MutableMap<ResolvedClass, MutableMap<ResolvedMethod, MutableSet<ResolvedReferenceTypeDeclaration>>> =
+        Collections.synchronizedMap(TreeMap(resolvedClassComparator))
 
     init {
         compilationUnits.forEachCompilationUnit(logger, ::processCU)
+        parseMethodImplementations()
     }
 
     private fun processCU(cu: CompilationUnit) {
         cu.findAll(ClassOrInterfaceDeclaration::class.java)
             .map { it.resolve() }
             .forEach { resolvedCU ->
-                val ancestors: List<ResolvedClass> = resolvedCU.getAllAncestors(ResolvedReferenceTypeDeclaration.breadthFirstFunc)
+                //Add ancestors (superclasses & superinterfaces) to the map
+                resolvedCU
+                    .getAllAncestors(ResolvedReferenceTypeDeclaration.breadthFirstFunc)
+                    .filterNot { it.isJavaLangEnum || it.isJavaLangObject }
+                    .forEach {
+                        subclassesMap.computeIfAbsent(it) { Collections.synchronizedList(arrayListOf()) }
+                            .add(resolvedCU)
+                    }
+            }
 
-                //For the top most implementations, inherited methods may indirectly implement an interface,
-                // so we need to take all methods and then check #declaringType to see where that implementation is
-                resolvedCU.allMethods.map { it.declaration }.filterIsInstance<JavaParserMethodDeclaration>().forEach forEachMethod@{ resolvedMethod: ResolvedMethod ->
-                    if (resolvedMethod.declaringType().qualifiedName == resolvedCU.qualifiedName) return@forEachMethod
+    }
 
-                    //TODO Abstract methods all appear as being implement by every subclasses
-                    // All inheritors of a given interface are bound to have the interface methods
-                    // Figure out a way to see if the *current class* (resolvedCU) has that method
-                    // This may be as simple as eliminating declaredMethods from allMethods
-                    // This would also eliminate the above check
-                    classToMethodImplementations
-                        .computeIfAbsent(resolvedMethod.declaringType()) { Collections.synchronizedMap(TreeMap(resolvedMethodComparator)) }
-                        .computeIfAbsent(resolvedMethod) { Collections.synchronizedSet(TreeSet(resolvedReferenceTypeDeclarationComparator)) }
-                        .add(resolvedCU)
-
-//                    val overriddenClasses = ancestors.filter { ancestor ->
-//                        ancestor.declaredMethods.any { ancestorMethod ->
-//                            resolvedMethod.name == ancestorMethod.name
-//                                    && resolvedMethod.numberOfParams == ancestorMethod.noParams
-//                                    && (0 until ancestorMethod.noParams).all { i ->
-//                                ancestorMethod.getParamType(i).isAssignableBy(resolvedMethod.getParam(i).type)
-//                            }
-//                        }
-//                    }
-//
-//                    overriddenClasses.forEach { overriddenClass ->
-//                        classToMethodImplementations
-//                            .computeIfAbsent(overriddenClass) { Collections.synchronizedMap(TreeMap(resolvedMethodComparator)) }
-//                            .computeIfAbsent(resolvedMethod) { Collections.synchronizedSet(TreeSet(resolvedReferenceTypeDeclarationComparator)) }
-//                            .add(resolvedCU) //TODO not sure if a Set is really needed, it seems to always contain a single item
-//                    }
-                }
-
-                ancestors.forEach {
-                    subclassesMap.computeIfAbsent(it) { Collections.synchronizedList(arrayListOf()) }.add(resolvedCU)
+    //After computing all subclasses, iterate on all superclasses (the map's keys),
+    // take each subclass's methods and check signature compared to the superclass method
+    private fun parseMethodImplementations() {
+        subclassesMap.forEach { (superclass, subclasses) ->
+            superclass.declaredMethods.forEach { superMethod ->
+                subclasses.forEach { subclass ->
+                    subclass.declaredMethods.forEach { subMethod ->
+                        if (isMethodCompatible(subMethod, superMethod)) {
+                            classToMethodImplementations
+                                .computeIfAbsent(superclass) { resolvedMethodComparator.createMap() }
+                                .computeIfAbsent(superMethod.declaration) { resolvedReferenceTypeDeclarationComparator.createSet() }
+                                .add(subclass)
+                        }
+                    }
                 }
             }
+        }
     }
+
+    private fun isMethodCompatible(subMethod: ResolvedMethodDeclaration, superMethod: MethodUsage): Boolean {
+        if (subMethod.name != superMethod.name) return false
+        if (subMethod.numberOfParams != superMethod.noParams) return false
+
+        return (0 until superMethod.noParams).all { i ->
+            superMethod.getParamType(i).isAssignableBy(subMethod.getParam(i).type)
+        }
+    }
+
+    private fun <K, V> Comparator<K>.createMap(): MutableMap<K, V> = Collections.synchronizedMap(TreeMap(this))
+    private fun <E> Comparator<E>.createSet(): MutableSet<E> = Collections.synchronizedSet(TreeSet(this))
 
     private val ResolvedMethodDeclaration.fixedDescriptor: String
         get() = buildString {
             append('(')
-            for(i in 0..<numberOfParams) {
+            for (i in 0..<numberOfParams) {
                 append(getParam(i).type.describe())
             }
             append(')')
--- src/test/kotlin/com/freya02/docs/SourceRootMetadataTest.kt
+++ src/test/kotlin/com/freya02/docs/SourceRootMetadataTest.kt
@@ -5,8 +5,6 @@ import com.freya02.bot.docs.metadata.SourceRootMetadata
 
 fun main() {
     //TODO IPermissionContainerManager is not in method implementations
-    //FIXED UpdateEvent#getOldValue is not in #allMethods as it is being shadowed by implementations using the same return type & signature
-    //TODO UpdateEvent#getOldValue is reported to be implemented in every subclass
     //TODO Remove duplicates from subclasses & possibly method implementations
     val metadata = SourceRootMetadata(Data.javadocsPath.resolve("JDA"))
 
