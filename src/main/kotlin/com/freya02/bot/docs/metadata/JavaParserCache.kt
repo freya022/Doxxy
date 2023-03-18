f110ef593d65f5a3a340440958266f8a43864f76
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-18 10:44:14 +0100

    Improve JavaParserCache

--- src/main/kotlin/com/freya02/bot/docs/metadata/JavaParserCache.kt
+++ src/main/kotlin/com/freya02/bot/docs/metadata/JavaParserCache.kt
@@ -1,29 +1,48 @@
 package com.freya02.bot.docs.metadata
 
+import com.github.javaparser.resolution.MethodUsage
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration
 import com.github.javaparser.resolution.declarations.ResolvedTypeDeclaration
 import com.github.javaparser.resolution.types.ResolvedReferenceType
 import java.util.*
 
 class JavaParserCache {
+    private class Cache<K, V>(container: MutableMap<K, V> = IdentityHashMap(), private val valueSupplier: (K) -> V) {
+        private val map: MutableMap<K, V> = Collections.synchronizedMap(container)
+        private var hits = 0
+
+        operator fun get(k: K): V {
+            hits++
+            return map.getOrPut(k) {
+                hits--
+                valueSupplier(k)
+            }
+        }
+
+        override fun toString(): String {
+            return "Size=${map.size}, hits=$hits"
+        }
+    }
+
     //JP keeps recreating these map's keys,
     // but these keys are still being used multiple times to a point where it is still beneficial
     // An IdentityHashMap is used as JP *might* not implement hashCode/equals correctly, while using a property as a key would be counterproductive
-    private val typeDeclarationQualifiedNames: MutableMap<ResolvedTypeDeclaration, String> = Collections.synchronizedMap(IdentityHashMap())
-    private val referenceTypeQualifiedNames: MutableMap<ResolvedReferenceType, String> = Collections.synchronizedMap(IdentityHashMap())
-    private val methodDeclarationQualifiedDescriptors: MutableMap<ResolvedMethodDeclaration, String> = Collections.synchronizedMap(IdentityHashMap())
+    private val typeDeclarationQualifiedNames = Cache<ResolvedTypeDeclaration, String> { it.qualifiedName }
+    private val referenceTypeQualifiedNames = Cache<ResolvedReferenceType, String> { it.qualifiedName }
+    private val methodDeclarationQualifiedDescriptors =
+        Cache<ResolvedMethodDeclaration, String> { it.qualifiedName + it.fixedDescriptor }
 
-    fun getQualifiedName(declaration: ResolvedTypeDeclaration): String {
-        return typeDeclarationQualifiedNames.getOrPut(declaration) { declaration.qualifiedName }
-    }
+    private val referenceTypeDeclaredMethods = Cache<ResolvedReferenceType, Set<MethodUsage>>() { it.declaredMethods }
 
-    fun getQualifiedName(declaration: ResolvedReferenceType): String {
-        return referenceTypeQualifiedNames.getOrPut(declaration) { declaration.qualifiedName }
-    }
+    fun getQualifiedName(declaration: ResolvedTypeDeclaration): String = typeDeclarationQualifiedNames[declaration]
 
-    fun getQualifiedDescriptor(declaration: ResolvedMethodDeclaration): String {
-        return methodDeclarationQualifiedDescriptors.getOrPut(declaration) { declaration.qualifiedName + declaration.fixedDescriptor }
-    }
+    fun getQualifiedName(declaration: ResolvedReferenceType): String = referenceTypeQualifiedNames[declaration]
+
+    fun getQualifiedDescriptor(declaration: ResolvedMethodDeclaration): String =
+        methodDeclarationQualifiedDescriptors[declaration]
+
+    fun getDeclaredMethods(declaration: ResolvedReferenceType): Set<MethodUsage> =
+        referenceTypeDeclaredMethods[declaration]
 
     private val ResolvedMethodDeclaration.fixedDescriptor: String
         get() = buildString {
