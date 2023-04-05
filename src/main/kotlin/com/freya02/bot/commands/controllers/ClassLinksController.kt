5d8c08da4a6ac83c583c12b94bc16c7bcec0ba1c
freya022 <41875020+freya022@users.noreply.github.com>
2023-04-05 15:36:09 +0200

    Move class links code to controller

---
+++ src/main/kotlin/com/freya02/bot/commands/controllers/ClassLinksController.kt
@@ -0,0 +1,151 @@
+package com.freya02.bot.commands.controllers
+
+import com.freya02.bot.docs.DocIndexMap
+import com.freya02.bot.docs.cached.CachedClass
+import com.freya02.bot.docs.index.DocIndex
+import com.freya02.bot.docs.metadata.ClassType
+import com.freya02.bot.docs.metadata.ImplementationIndex
+import com.freya02.bot.utils.joinLengthyString
+import com.freya02.botcommands.api.components.Components
+import com.freya02.botcommands.api.components.event.ButtonEvent
+import com.freya02.botcommands.api.components.event.StringSelectEvent
+import com.freya02.botcommands.api.core.ServiceContainer
+import com.freya02.botcommands.api.core.annotations.BService
+import dev.minn.jda.ktx.interactions.components.row
+import dev.minn.jda.ktx.messages.MessageCreate
+import dev.minn.jda.ktx.messages.reply_
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.interactions.InteractionHook
+import net.dv8tion.jda.api.interactions.components.ItemComponent
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
+import net.dv8tion.jda.api.interactions.components.selections.SelectMenu
+import net.dv8tion.jda.api.utils.messages.MessageEditData
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.minutes
+
+@BService
+class ClassLinksController(
+    serviceContainer: ServiceContainer,
+    private val componentsService: Components,
+    private val docIndexMap: DocIndexMap
+) {
+    private val logger = KotlinLogging.logger { }
+    private val commonDocsController: CommonDocsController by serviceContainer.lazy()
+
+    context(MutableList<ItemComponent>)
+    fun addCachedClassComponents(cachedDoc: CachedClass, originalHook: InteractionHook?) {
+        val index = docIndexMap[cachedDoc.source]!!
+        val clazz = runBlocking {
+            index.implementationIndex.getClass(cachedDoc.name)
+        }
+
+        if (clazz != null) {
+            if (cachedDoc.subclasses.isNotEmpty()) {
+                val decorations = clazz.classType.subDecorations
+                componentsService.ephemeralButton(ButtonStyle.SECONDARY, decorations.label, decorations.emoji) {
+                    timeout(5.minutes)
+                    bindTo { sendClassLinks(it, originalHook, index, clazz, clazz.getSubclasses(), decorations) }
+                }.also { add(it) }
+            }
+
+            if (cachedDoc.superclasses.isNotEmpty()) {
+                val decorations = clazz.classType.superDecorations
+                componentsService.ephemeralButton(ButtonStyle.SECONDARY, decorations.label, decorations.emoji) {
+                    timeout(5.minutes)
+                    bindTo { sendClassLinks(it, originalHook, index, clazz, clazz.getSuperclasses(), decorations) }
+                }.also { add(it) }
+            }
+        } else {
+            logger.trace("Found no metadata for ${cachedDoc.name}")
+        }
+    }
+
+    private suspend fun sendClassLinks(
+        event: ButtonEvent,
+        originalHook: InteractionHook?,
+        index: DocIndex,
+        clazz: ImplementationIndex.Class,
+        classes: List<ImplementationIndex.Class>,
+        decorations: ClassType.Decorations
+    ) {
+        MessageCreate {
+            val (apiClasses, internalClasses) = classes.sortedBy { it.className }.partition { it.hasClassDoc() }
+
+            if (internalClasses.isNotEmpty()) {
+                embed {
+                    author(name = "${decorations.title} - ${clazz.className}", iconUrl = decorations.emoji.asCustom().imageUrl)
+                    description = internalClasses
+                        .joinLengthyString(
+                            separator = ", ",
+                            truncated = " and more...",
+                            lengthLimit = MessageEmbed.DESCRIPTION_MAX_LENGTH
+                        ) { internalClass -> "[${internalClass.className}](${internalClass.sourceLink})" }
+                }
+            }
+
+            if (apiClasses.isNotEmpty()) {
+                components += apiClasses.take(SelectMenu.OPTIONS_MAX_AMOUNT * 5)
+                    .chunked(SelectMenu.OPTIONS_MAX_AMOUNT) { superclassesChunk ->
+                        row(componentsService.ephemeralStringSelectMenu {
+                            if (originalHook != null) {
+                                timeout(originalHook.expirationTimestamp - System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                            } else {
+                                timeout(5.minutes)
+                            }
+
+                            val slashUserId = event.message.interaction!!.user.idLong
+                            bindTo { selectEvent -> onClassLinkSelect(
+                                selectEvent,
+                                slashUserId,
+                                index,
+                                originalHook,
+                                event
+                            ) }
+
+                            val firstChar = superclassesChunk.first().className.first()
+                            val lastChar = superclassesChunk.last().className.first()
+                            placeholder = "${decorations.placeholder} ($firstChar-$lastChar)"
+
+                            superclassesChunk.forEach {
+                                addOption(it.className, it.className, it.classType.emoji)
+                            }
+                        })
+                    }
+            }
+        }.also { event.reply(it).setEphemeral(true).queue() }
+    }
+
+    private suspend fun onClassLinkSelect(
+        selectEvent: StringSelectEvent,
+        slashUserId: Long,
+        index: DocIndex,
+        originalHook: InteractionHook?, //probably from /docs or similar
+        buttonEvent: ButtonEvent
+    ) {
+        val selectedClassName = selectEvent.values.single()
+        val isSameCaller = selectEvent.user.idLong == slashUserId
+        val cachedDoc = index.getClassDoc(selectedClassName)
+            ?: return selectEvent.reply_("This class no longer exists", ephemeral = true).queue()
+
+        val createData = commonDocsController.getDocMessageData(
+            originalHook,
+            selectEvent.member!!,
+            ephemeral = !isSameCaller,
+            showCaller = false,
+            cachedDoc
+        )
+
+        if (isSameCaller) {
+            selectEvent.deferEdit().queue()
+            if (originalHook != null) {
+                originalHook.editOriginal(MessageEditData.fromCreateData(createData)).queue()
+            } else {
+                buttonEvent.message.editMessage(MessageEditData.fromCreateData(createData)).queue()
+            }
+        } else {
+            selectEvent.reply(createData).setEphemeral(true).queue()
+        }
+    }
+}
--- src/main/kotlin/com/freya02/bot/commands/controllers/CommonDocsController.kt
+++ src/main/kotlin/com/freya02/bot/commands/controllers/CommonDocsController.kt
@@ -7,43 +7,33 @@ import com.freya02.bot.docs.cached.CachedClass
 import com.freya02.bot.docs.cached.CachedDoc
 import com.freya02.bot.docs.index.DocIndex
 import com.freya02.bot.docs.index.DocSuggestion
-import com.freya02.bot.docs.metadata.ClassType
-import com.freya02.bot.docs.metadata.ImplementationIndex
 import com.freya02.bot.utils.Emojis
-import com.freya02.bot.utils.joinLengthyString
 import com.freya02.botcommands.api.components.Components
 import com.freya02.botcommands.api.components.data.InteractionConstraints
-import com.freya02.botcommands.api.components.event.ButtonEvent
-import com.freya02.botcommands.api.components.event.StringSelectEvent
 import com.freya02.botcommands.api.core.annotations.BService
 import com.freya02.botcommands.api.pagination.menu.ChoiceMenuBuilder
 import com.freya02.botcommands.api.utils.ButtonContent
 import com.freya02.docs.DocSourceType
 import com.freya02.docs.data.TargetType
-import dev.minn.jda.ktx.interactions.components.row
 import dev.minn.jda.ktx.messages.Embed
-import dev.minn.jda.ktx.messages.MessageCreate
-import dev.minn.jda.ktx.messages.reply_
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import net.dv8tion.jda.api.entities.Member
-import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.UserSnowflake
 import net.dv8tion.jda.api.interactions.InteractionHook
 import net.dv8tion.jda.api.interactions.components.ItemComponent
 import net.dv8tion.jda.api.interactions.components.buttons.Button
-import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
 import net.dv8tion.jda.api.interactions.components.selections.SelectMenu
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder
 import net.dv8tion.jda.api.utils.messages.MessageCreateData
 import net.dv8tion.jda.api.utils.messages.MessageCreateRequest
-import net.dv8tion.jda.api.utils.messages.MessageEditData
-import java.util.concurrent.TimeUnit
-import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 
 @BService
-class CommonDocsController(private val componentsService: Components, private val docIndexMap: DocIndexMap) {
+class CommonDocsController(
+    private val componentsService: Components,
+    private val docIndexMap: DocIndexMap,
+    private val classLinksController: ClassLinksController
+) {
     private val logger = KotlinLogging.logger { }
 
     fun buildDocSuggestionsMenu(docIndex: DocIndex, suggestions: List<DocSuggestion>, user: UserSnowflake, block: ChoiceMenuBuilder<DocSuggestion>.() -> Unit) =
@@ -96,32 +86,8 @@ class CommonDocsController(private val componentsService: Components, private va
         val list: List<ItemComponent> = buildList {
             cachedDoc.sourceLink?.let { sourceLink -> add(Button.link(sourceLink, "Source")) }
 
-            if (cachedDoc is CachedClass) {
-                val index = docIndexMap[cachedDoc.source]!!
-                val clazz = runBlocking {
-                    index.implementationIndex.getClass(cachedDoc.name)
-                }
-
-                if (clazz != null) {
-                    if (cachedDoc.subclasses.isNotEmpty()) {
-                        val decorations = clazz.classType.subDecorations
-                        componentsService.ephemeralButton(ButtonStyle.SECONDARY, decorations.label, decorations.emoji) {
-                            timeout(5.minutes)
-                            bindTo { sendClassLinks(it, originalHook, index, clazz, clazz.getSubclasses(), decorations) }
-                        }.also { add(it) }
-                    }
-
-                    if (cachedDoc.superclasses.isNotEmpty()) {
-                        val decorations = clazz.classType.superDecorations
-                        componentsService.ephemeralButton(ButtonStyle.SECONDARY, decorations.label, decorations.emoji) {
-                            timeout(5.minutes)
-                            bindTo { sendClassLinks(it, originalHook, index, clazz, clazz.getSuperclasses(), decorations) }
-                        }.also { add(it) }
-                    }
-                } else {
-                    logger.trace("Found no metadata for ${cachedDoc.name}")
-                }
-            }
+            if (cachedDoc is CachedClass)
+                classLinksController.addCachedClassComponents(cachedDoc, originalHook)
 
             if (!ephemeral) add(componentsService.messageDeleteButton(caller))
         }
@@ -159,94 +125,6 @@ class CommonDocsController(private val componentsService: Components, private va
 
                 addActionRow(selectMenu)
             }
-        }
-    }
-
-    private suspend fun sendClassLinks(
-        event: ButtonEvent,
-        originalHook: InteractionHook?,
-        index: DocIndex,
-        clazz: ImplementationIndex.Class,
-        classes: List<ImplementationIndex.Class>,
-        decorations: ClassType.Decorations
-    ) {
-        MessageCreate {
-            val (apiClasses, internalClasses) = classes.sortedBy { it.className }.partition { it.hasClassDoc() }
-
-            if (internalClasses.isNotEmpty()) {
-                embed {
-                    author(name = "${decorations.title} - ${clazz.className}", iconUrl = decorations.emoji.asCustom().imageUrl)
-                    description = internalClasses
-                        .joinLengthyString(
-                            separator = ", ",
-                            truncated = " and more...",
-                            lengthLimit = MessageEmbed.DESCRIPTION_MAX_LENGTH
-                        ) { internalClass -> "[${internalClass.className}](${internalClass.sourceLink})" }
-                }
-            }
-
-            if (apiClasses.isNotEmpty()) {
-                components += apiClasses.take(SelectMenu.OPTIONS_MAX_AMOUNT * 5)
-                    .chunked(SelectMenu.OPTIONS_MAX_AMOUNT) { superclassesChunk ->
-                        row(componentsService.ephemeralStringSelectMenu {
-                            if (originalHook != null) {
-                                println("Expire in ${(originalHook.expirationTimestamp - System.currentTimeMillis()).milliseconds.inWholeMinutes} minutes")
-                                timeout(originalHook.expirationTimestamp - System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-                            } else {
-                                timeout(5.minutes)
-                            }
-
-                            val slashUserId = event.message.interaction!!.user.idLong
-                            bindTo { selectEvent -> onClassLinkSelect(
-                                selectEvent,
-                                slashUserId,
-                                index,
-                                originalHook,
-                                event
-                            ) }
-
-                            val firstChar = superclassesChunk.first().className.first()
-                            val lastChar = superclassesChunk.last().className.first()
-                            placeholder = "${decorations.placeholder} ($firstChar-$lastChar)"
-
-                            superclassesChunk.forEach {
-                                addOption(it.className, it.className, it.classType.emoji)
-                            }
-                        })
-                    }
-            }
-        }.also { event.reply(it).setEphemeral(true).queue() }
-    }
-
-    private suspend fun onClassLinkSelect(
-        selectEvent: StringSelectEvent,
-        slashUserId: Long,
-        index: DocIndex,
-        originalHook: InteractionHook?, //probably from /docs or similar
-        buttonEvent: ButtonEvent
-    ) {
-        val selectedClassName = selectEvent.values.single()
-        val isSameCaller = selectEvent.user.idLong == slashUserId
-        val cachedDoc = index.getClassDoc(selectedClassName)
-            ?: return selectEvent.reply_("This class no longer exists", ephemeral = true).queue()
-
-        val createData = getDocMessageData(
-            originalHook,
-            selectEvent.member!!,
-            ephemeral = !isSameCaller,
-            showCaller = false,
-            cachedDoc
-        )
-
-        if (isSameCaller) {
-            selectEvent.deferEdit().queue()
-            if (originalHook != null) {
-                originalHook.editOriginal(MessageEditData.fromCreateData(createData)).queue()
-            } else {
-                buttonEvent.message.editMessage(MessageEditData.fromCreateData(createData)).queue()
-            }
-        } else {
-            selectEvent.reply(createData).setEphemeral(true).queue()
         }
     }
 }
