01f815522f517c54a3ce3ffeb177b0f2979951f0
freya02 <41875020+freya022@users.noreply.github.com>
2023-04-07 17:50:38 +0200

    Warn when cache hits are insufficient or when expected size is lower than actual size

--- src/main/kotlin/com/freya02/bot/docs/metadata/parser/JavaParserCache.kt
+++ src/main/kotlin/com/freya02/bot/docs/metadata/parser/JavaParserCache.kt
@@ -16,9 +16,18 @@ import kotlin.reflect.jvm.javaField
 import kotlin.reflect.jvm.jvmErasure
 
 class JavaParserCache {
-    private class Cache<T, K, V>(container: MutableMap<K, V> = IdentityHashMap(), private val keySupplier: (T) -> K, private val valueSupplier: (T) -> V) {
+    private class Cache<T, K, V>(
+        private val expectedItems: Int,
+        container: MutableMap<K, V> = IdentityHashMap(calcMaxSize(expectedItems)),
+        private val keySupplier: (T) -> K,
+        private val valueSupplier: (T) -> V
+    ) {
         private val map: MutableMap<K, V> = Collections.synchronizedMap(container)
-        private var hits = 0
+        var hits = 0
+            private set
+        val expectedSize = calcMaxSize(expectedItems)
+        val size: Int
+            get() = map.size
 
         operator fun get(t: T): V {
             hits++
@@ -33,39 +42,39 @@ class JavaParserCache {
         }
     }
 
-    private fun <K, V> Cache(container: MutableMap<K, V> = IdentityHashMap(), valueSupplier: (K) -> V) =
-        Cache(container, { it }, valueSupplier)
+    private fun <K, V> Cache(expectedMaxSize: Int, container: MutableMap<K, V> = IdentityHashMap(), valueSupplier: (K) -> V) =
+        Cache(expectedMaxSize, container, { it }, valueSupplier)
 
     private fun <K, V> Cache(expectedMaxSize: Int, valueSupplier: (K) -> V)
-        = Cache(IdentityHashMap(expectedMaxSize), { it }, valueSupplier)
+        = Cache(expectedMaxSize, keySupplier = { it }, valueSupplier = valueSupplier)
 
-    private fun <K, V> Cache(comparator: Comparator<K>, valueSupplier: (K) -> V)
-        = Cache(TreeMap(comparator), { it }, valueSupplier)
+    private fun <K, V> Cache(expectedMaxSize: Int, comparator: Comparator<K>, valueSupplier: (K) -> V)
+        = Cache(expectedMaxSize, TreeMap(comparator), { it }, valueSupplier)
 
     //JP keeps recreating these map's keys,
     // but these keys are still being used multiple times to a point where it is still beneficial
     // An IdentityHashMap is used as JP *might* not implement hashCode/equals correctly, while using a property as a key would be counterproductive
-    private val typeDeclarationQualifiedNames = Cache<ResolvedTypeDeclaration, String>(calcMaxSize(10232.0)) { it.qualifiedName }
-    private val referenceTypeQualifiedNames = Cache<ResolvedReferenceType, String>(calcMaxSize(3869.0)) { it.qualifiedName }
-    private val methodDeclarationSignatures = Cache<ResolvedMethodDeclaration, String>(calcMaxSize(4712.0)) {
+    private val typeDeclarationQualifiedNames = Cache<ResolvedTypeDeclaration, String>(10232) { it.qualifiedName }
+    private val referenceTypeQualifiedNames = Cache<ResolvedReferenceType, String>(5736) { it.qualifiedName }
+    private val methodDeclarationSignatures = Cache<ResolvedMethodDeclaration, String>(4712) {
         it.erasedSimpleSignature
     }
 
     //A Comparator is used as the map keys are being recreated everytime, an IdentityHashMap would not work
     private val referenceTypeLightDeclaredMethods =
-        Cache<ResolvedReferenceType, Set<ResolvedMethodDeclaration>>(Comparator.comparing { getQualifiedName(it) }) { it.lightDeclaredMethods }
+        Cache<ResolvedReferenceType, Set<ResolvedMethodDeclaration>>(345, Comparator.comparing { getQualifiedName(it) }) { it.lightDeclaredMethods }
     private val methodDeclarationDeclaringTypes =
-        Cache<ResolvedMethodDeclaration, ResolvedReferenceTypeDeclaration>(calcMaxSize(9690.0)) { it.declaringType() }
+        Cache<ResolvedMethodDeclaration, ResolvedReferenceTypeDeclaration>(9690) { it.declaringType() }
 
     private val parameterDeclarationTypes =
         Cache<ResolvedParameterDeclaration, Parameter, ResolvedType>(
-            container = IdentityHashMap(calcMaxSize(2057.0)),
+            expectedItems = 2057,
             keySupplier = { it.toAst(Parameter::class.java).get() },
             valueSupplier = { it.type }
         )
 
     private val referenceTypeDirectAncestors =
-        Cache<ResolvedReferenceType, List<ResolvedReferenceType>>(Comparator.comparing { getQualifiedName(it) }) { it.directAncestors }
+        Cache<ResolvedReferenceType, List<ResolvedReferenceType>>(365, Comparator.comparing { getQualifiedName(it) }) { it.directAncestors }
 
     fun getQualifiedName(declaration: ResolvedTypeDeclaration): String = typeDeclarationQualifiedNames[declaration]
 
@@ -85,21 +94,33 @@ class JavaParserCache {
             = referenceTypeDirectAncestors[declaration]
 
     fun logCaches() {
-        logger.info("Cache info:")
+        logger.trace("Cache info:")
         this::class.declaredMemberProperties
             .filter { it.returnType.jvmErasure == Cache::class }
             .forEach {
                 val cache: Cache<*, *, *> = it.javaField?.get(this) as? Cache<*, *, *>
                     ?: return logger.warn("Could not get field '${it.name}'")
-                logger.info("${it.name}: $cache")
+                logger.trace("${it.name}: $cache")
+
+                //Cache's map was expanded
+                if (cache.expectedSize < cache.size) {
+                    logger.warn { "Cache ${it.name} was configured to cache ${cache.expectedSize} items but caches ${cache.size} items" }
+                }
+
+                //Cache is not used
+                if (cache.hits < cache.size) {
+                    logger.warn { "Cache ${it.name} has only ${cache.hits} but caches ${cache.size} items" }
+                }
             }
     }
 
-    //Used to avoid resizing IdentityHashMap, the sizes comes from the map's sizes when parsing JDA 5 beta 5
-    // Takes the nearest power of 2 toward positive infinite
-    private fun calcMaxSize(size: Double) = 1 shl ceil(log2(size)).toInt()
+
 
     private companion object {
         private val logger = KotlinLogging.logger { }
+
+        //Used to avoid resizing IdentityHashMap, the sizes comes from the map's sizes when parsing JDA 5 beta 5
+        // Takes the nearest power of 2 toward positive infinite
+        private fun calcMaxSize(size: Int) = 1 shl ceil(log2(size.toDouble())).toInt()
     }
 }
