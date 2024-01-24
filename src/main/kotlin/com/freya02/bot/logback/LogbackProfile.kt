c9c4cb8064ba6d8f49f046c8131f87593735e24e
freya02 <41875020+freya022@users.noreply.github.com>
2024-01-24 21:21:05 +0100

    Fix typo in LogbackProfile#PROD_WITH_FILE

--- src/main/kotlin/com/freya02/bot/logback/LogbackProfile.kt
+++ src/main/kotlin/com/freya02/bot/logback/LogbackProfile.kt
@@ -3,5 +3,5 @@ package com.freya02.bot.logback
 enum class LogbackProfile(val pathFragment: String) {
     DEV("dev/console"),
     DEV_WITH_FILE("dev/file"),
-    PROD_WITH_FILE("dev/file")
+    PROD_WITH_FILE("prod/file")
 }
