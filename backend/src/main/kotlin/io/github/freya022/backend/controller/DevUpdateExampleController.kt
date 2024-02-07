b3c451f76fb7067ae1581dab74bfd3325e2767c2
freya02 <41875020+freya022@users.noreply.github.com>
2024-02-07 20:12:24 +0100

    Keep /examples/update always enabled
    Until a webhook or any other event type is used

--- backend/src/main/kotlin/io/github/freya022/backend/controller/DevUpdateExampleController.kt
+++ backend/src/main/kotlin/io/github/freya022/backend/controller/DevUpdateExampleController.kt
@@ -1,11 +1,10 @@
 package io.github.freya022.backend.controller
 
 import io.github.freya022.backend.service.ExamplesService
-import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Profile("dev")
+//@Profile("dev") //TODO enable once updates are using webhooks
 @RestController
 class DevUpdateExampleController(
     private val examplesService: ExamplesService
