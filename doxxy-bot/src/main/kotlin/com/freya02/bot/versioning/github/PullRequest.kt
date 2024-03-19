760243ad5cdbd6890b1bd32e36eef1d1eaa605dd
freya02 <41875020+freya022@users.noreply.github.com>
2024-03-19 22:39:33 +0100

    Rework pull request fuzzy matching

--- doxxy-bot/src/main/kotlin/com/freya02/bot/versioning/github/PullRequest.kt
+++ doxxy-bot/src/main/kotlin/com/freya02/bot/versioning/github/PullRequest.kt
@@ -2,9 +2,7 @@ package com.freya02.bot.versioning.github
 
 import com.freya02.bot.utils.Utils.truncate
 import com.freya02.bot.versioning.ArtifactInfo
-import io.github.freya022.botcommands.api.commands.application.slash.autocomplete.AutocompleteAlgorithms
-import io.github.freya022.botcommands.api.commands.application.slash.autocomplete.FuzzyResult
-import io.github.freya022.botcommands.api.commands.application.slash.autocomplete.ToStringFunction
+import info.debatty.java.stringsimilarity.LongestCommonSubsequence
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.Command
 import net.dv8tion.jda.api.interactions.commands.Command.Choice
@@ -31,6 +29,8 @@ data class PullRequest(
         }
 
     companion object {
+        private val lcs = LongestCommonSubsequence()
+
         fun fromData(data: DataObject): PullRequest? {
             val head = data.getObject("head")
             if (head.isNull("repo")) { //If we don't have that then the head repo (the fork) doesn't exist
@@ -59,37 +59,23 @@ data class PullRequest(
         }
 
         fun Collection<PullRequest>.toAutocompleteChoices(event: CommandAutoCompleteInteractionEvent): List<Choice> {
-            val sortedPullRequests = if (event.focusedOption.value.isBlank()) {
-                sortedByDescending { it.number }
-            } else {
-                fuzzyMatching(
-                    //Don't autocomplete based on the branch number
-                    toStringFunction = { pr: PullRequest -> pr.title + pr.authorName },
-                    query = event.focusedOption.value
-                ).map { fuzzyResult -> fuzzyResult.item }
-            }
+            val query = event.focusedOption.value
+            val sortedPullRequests = when {
+                query.isBlank() -> sortedByDescending { it.number }
 
-            return sortedPullRequests.map { r -> r.toChoice() }
+                query.first().isDigit() ->
+                    filter { it.number.toString().startsWith(query) }
+                        .sortedByDescending { it.number }
+
+                else -> {
+                    val lowercaseQuery = query.lowercase()
+                    sortedByDescending { lcs.length(lowercaseQuery, it.title.lowercase()) }
+                }
+            }.take(OptionData.MAX_CHOICES)
+
+            return sortedPullRequests.map { it.toChoice() }
         }
 
         private fun PullRequest.toChoice() = Choice(asHumanDescription, number.toLong())
-
-        private fun Collection<PullRequest>.fuzzyMatching(
-            toStringFunction: ToStringFunction<PullRequest>,
-            query: String
-        ): List<FuzzyResult<PullRequest>> {
-            if (query.firstOrNull()?.isDigit() == true) {
-                return filter { it.number.toString().startsWith(query) }
-                    .take(OptionData.MAX_CHOICES)
-                    .map { FuzzyResult(it, "", 0.0) }
-            }
-
-            return sortedWith(Comparator.comparingInt { obj: PullRequest -> obj.number }.reversed()).let {
-                when {
-                    query.isBlank() -> take(OptionData.MAX_CHOICES).map { FuzzyResult(it, "", 0.0) }
-                    else -> AutocompleteAlgorithms.fuzzyMatching(this, toStringFunction, query).take(OptionData.MAX_CHOICES)
-                }
-            }
-        }
     }
 }
