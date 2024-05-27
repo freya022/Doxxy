d4106c21a45ba0cdd30b32cc535bba908347c255
freya02 <41875020+freya022@users.noreply.github.com>
2024-05-27 16:50:17 +0200

    Remove ignored timeout parameter on CoroutineEventManager

--- doxxy-bot/src/main/kotlin/com/freya02/bot/CoroutineEventManagerSupplier.kt
+++ doxxy-bot/src/main/kotlin/com/freya02/bot/CoroutineEventManagerSupplier.kt
@@ -4,12 +4,11 @@ import dev.minn.jda.ktx.events.CoroutineEventManager
 import io.github.freya022.botcommands.api.core.ICoroutineEventManagerSupplier
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.freya022.botcommands.api.core.utils.namedDefaultScope
-import kotlin.time.Duration.Companion.minutes
 
 @BService
 class CoroutineEventManagerSupplier : ICoroutineEventManagerSupplier {
     override fun get(): CoroutineEventManager {
         val scope = namedDefaultScope("Doxxy coroutine", 4)
-        return CoroutineEventManager(scope, 1.minutes)
+        return CoroutineEventManager(scope)
     }
 }
