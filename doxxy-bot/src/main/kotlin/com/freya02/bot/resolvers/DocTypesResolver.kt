5723ed3d41aa420132062f0c5fa89de689e3511d
freya02 <41875020+freya022@users.noreply.github.com>
2024-07-16 16:33:28 +0200

    Fix command info imports

--- doxxy-bot/src/main/kotlin/com/freya02/bot/resolvers/DocTypesResolver.kt
+++ doxxy-bot/src/main/kotlin/com/freya02/bot/resolvers/DocTypesResolver.kt
@@ -1,10 +1,10 @@
 package com.freya02.bot.resolvers
 
 import com.freya02.bot.docs.index.DocTypes
+import io.github.freya022.botcommands.api.commands.application.slash.SlashCommandInfo
 import io.github.freya022.botcommands.api.core.service.annotations.Resolver
 import io.github.freya022.botcommands.api.parameters.ClassParameterResolver
 import io.github.freya022.botcommands.api.parameters.resolvers.SlashParameterResolver
-import io.github.freya022.botcommands.internal.commands.application.slash.SlashCommandInfo
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.interactions.commands.Command.Choice
 import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload
