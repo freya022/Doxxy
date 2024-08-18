e91d757bb8ba84c208d5e1cdbda250ab0226b9c4
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-18 20:14:20 +0200

    Use OpenTelemetrySourceSupplier

--- doxxy-bot/src/main/kotlin/com/freya02/bot/config/DatabaseSource.kt
+++ doxxy-bot/src/main/kotlin/com/freya02/bot/config/DatabaseSource.kt
@@ -1,32 +1,20 @@
 package com.freya02.bot.config
 
 import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import io.github.freya022.botcommands.api.core.db.ConnectionSupplier
+import io.github.freya022.botcommands.api.core.db.OpenTelemetrySourceSupplier
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.opentelemetry.api.OpenTelemetry
-import io.opentelemetry.instrumentation.jdbc.datasource.JdbcTelemetry
 import org.flywaydb.core.Flyway
-import java.sql.Connection
-import java.sql.SQLException
-import java.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 private val logger = KotlinLogging.logger { }
 
 @BService
-class DatabaseSource(config: Config, openTelemetry: OpenTelemetry) : ConnectionSupplier {
-    private val hikariConfig = HikariConfig().apply {
-        val databaseConfig = config.databaseConfig
-        jdbcUrl = databaseConfig.url
-        username = databaseConfig.user
-        password = databaseConfig.password
-
-        maximumPoolSize = 2
-        leakDetectionThreshold = 10.seconds.inWholeMilliseconds
-    }
-    private val source = JdbcTelemetry.builder(openTelemetry).build().wrap(HikariDataSource(hikariConfig))
+class DatabaseSource(
+    databaseConfig: DatabaseConfig,
+    openTelemetry: OpenTelemetry
+) : OpenTelemetrySourceSupplier(openTelemetry, createConfig(databaseConfig)) {
 
     init {
         //Migrate BC tables
@@ -44,12 +32,13 @@ class DatabaseSource(config: Config, openTelemetry: OpenTelemetry) : ConnectionS
         .validateMigrationNaming(true)
         .loggers("slf4j")
         .load()
+}
 
-    override val maxConnections: Int
-        get() = hikariConfig.maximumPoolSize
-    override val maxTransactionDuration: Duration
-        get() = Duration.ofMillis(hikariConfig.leakDetectionThreshold)
+private fun createConfig(databaseConfig: DatabaseConfig) = HikariConfig().apply {
+    jdbcUrl = databaseConfig.url
+    username = databaseConfig.user
+    password = databaseConfig.password
 
-    @Throws(SQLException::class)
-    override fun getConnection(): Connection = source.connection
+    maximumPoolSize = 2
+    leakDetectionThreshold = 10.seconds.inWholeMilliseconds
 }
