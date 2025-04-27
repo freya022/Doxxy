8dd335f64a0c02b27e3993797ca582f146a258d8
freya022 <41875020+freya022@users.noreply.github.com>
2025-04-27 21:14:42 +0200

    Add versions Gradle plugin

--- buildSrc/src/main/kotlin/doxxy-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/doxxy-conventions.gradle.kts
@@ -1,7 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("jvm")
+
+    id("com.github.ben-manes.versions")
 }
 
 //group = "com.freya02"
@@ -10,6 +13,22 @@ version = "2.5"
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
+}
+
+private val matchers: Map<String, Regex> = mapOf(
+    "org.junit.jupiter" to Regex(".+-M\\d+"),
+    "net.dv8tion" to Regex(".+_DEV"),
+    "com.fasterxml.jackson" to Regex(".+-rc.+"),
+    "org.slf4j" to Regex(".+-alpha.+"),
+    "io.ktor" to Regex(".+-beta.+"),
+    "org.jetbrains.kotlin" to Regex(".+-(?:Beta|RC).*"),
+    "org.jetbrains.kotlinx" to Regex(".+-(?:Beta|RC).*"),
+)
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        matchers.any { (packagePrefix, regex) -> packagePrefix in candidate.group && candidate.version.matches(regex) }
+    }
 }
 
 dependencies {
