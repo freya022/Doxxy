6424da7a6fe07cbfb69532aab794254caa017d96
freya02 <41875020+freya022@users.noreply.github.com>
2025-05-03 16:02:43 +0200

    Typo

--- doxxy-commons/src/main/kotlin/dev/freya02/doxxy/common/Directories.kt
+++ doxxy-commons/src/main/kotlin/dev/freya02/doxxy/common/Directories.kt
@@ -6,6 +6,6 @@ import kotlin.io.path.Path
 object Directories {
 
     val javadocs = Path(Env["JAVADOCS_DIRECTORY"])
-    val jdaFork: Path = Path(Env["JDA-Fork_DIRECTORY"])
+    val jdaFork: Path = Path(Env["JDA-FORK_DIRECTORY"])
     val pageCache: Path = Path(Env["PAGE_CACHE_DIRECTORY"])
 }
