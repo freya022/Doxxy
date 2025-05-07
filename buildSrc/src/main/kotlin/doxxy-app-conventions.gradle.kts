8fe2519f7606813d98d84e0b194dd97e460b82e8
freya02 <41875020+freya022@users.noreply.github.com>
2025-05-07 12:46:03 +0200

    Mark JIB tasks as incompatible with the configuration cache

--- buildSrc/src/main/kotlin/doxxy-app-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/doxxy-app-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.cloud.tools.jib.gradle.JibTask
+
 plugins {
     kotlin("jvm")
     id("com.google.cloud.tools.jib")
@@ -6,4 +8,8 @@ plugins {
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.16")
     implementation("dev.reformator.stacktracedecoroutinator:stacktrace-decoroutinator-jvm:2.4.8")
+}
+
+tasks.withType<JibTask>().configureEach {
+    notCompatibleWithConfigurationCache("because https://github.com/GoogleContainerTools/jib/issues/3132")
 }
