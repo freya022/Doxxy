0e67f8a649f6e259fd1586d57167c33983cb24f8
freya02 <41875020+freya022@users.noreply.github.com>
2025-05-08 12:13:14 +0200

    Move MethodDocParameter construction to itself

--- doxxy-docs/src/main/kotlin/dev/freya02/doxxy/docs/declarations/MethodDocParameter.kt
+++ doxxy-docs/src/main/kotlin/dev/freya02/doxxy/docs/declarations/MethodDocParameter.kt
@@ -1,3 +1,39 @@
 package dev.freya02.doxxy.docs.declarations
 
-data class MethodDocParameter(val annotations: Set<String>, val type: String, val simpleType: String, val name: String)
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ast.nodeTypes.NodeWithTypeArguments
+
+@ConsistentCopyVisibility
+data class MethodDocParameter private constructor(
+    val annotations: Set<String>,
+    val type: String,
+    val simpleType: String,
+    val name: String
+) {
+
+    internal companion object {
+
+        private val parser = JavaParser()
+
+        internal fun parse(source: String): MethodDocParameter {
+            val parsedParameter = synchronized(parser) {
+                parser.parseParameter(source).result.get()
+            }.also {
+                val type = it.type
+                if (type is NodeWithTypeArguments<*>) type.removeTypeArguments()
+            }
+
+            val typeAsString = parsedParameter.typeAsString
+
+            val annotations = parsedParameter.annotations.map { it.nameAsString }.toSet()
+            val type = typeAsString + if (parsedParameter.isVarArgs) "..." else ""
+            val simpleType = run {
+                val startIndex = typeAsString.indexOfFirst { it.isUpperCase() }.coerceAtLeast(0)
+                typeAsString.substring(startIndex) + if (parsedParameter.isVarArgs) "..." else ""
+            }
+            val name = parsedParameter.nameAsString
+
+            return MethodDocParameter(annotations, type, simpleType, name)
+        }
+    }
+}
