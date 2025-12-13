52d56b09ee8eaac6f31403682d66b2a86644e84f
freya02 <41875020+freya022@users.noreply.github.com>
2025-12-13 15:17:06 +0100

    Extract sub/superscript HTML visitor

--- doxxy-bot/src/main/kotlin/dev/freya02/doxxy/bot/docs/render/Markdown.kt
+++ doxxy-bot/src/main/kotlin/dev/freya02/doxxy/bot/docs/render/Markdown.kt
@@ -9,38 +9,38 @@ import org.jsoup.nodes.Element
 import org.jsoup.nodes.Node
 import org.jsoup.select.NodeVisitor
 
-internal fun JavadocElement.toMarkdown(): String {
-    targetElement.traverse(object : NodeVisitor {
-        override fun head(node: Node, depth: Int) {
-            if (node !is Element) return
+private object SubSuperScriptsVisitor : NodeVisitor {
+    override fun head(node: Node, depth: Int) {
+        if (node !is Element) return
 
-            if (!node.tagName().equals("a", ignoreCase = true)) {
-                if (node.tagName().equals("sub", ignoreCase = true)) {
-                    node.text().replaceScriptCharactersOrNull(SubSuperScripts.subscripts)?.let { node.text(it) }
-                } else if (node.tagName().equals("sup", ignoreCase = true)) {
-                    node.text().replaceScriptCharactersOrNull(SubSuperScripts.superscripts)?.let { node.text(it) }
-                }
-
-                return
+        if (!node.tagName().equals("a", ignoreCase = true)) {
+            if (node.tagName().equals("sub", ignoreCase = true)) {
+                node.text().replaceScriptCharactersOrNull(SubSuperScripts.subscripts)?.let { node.text(it) }
+            } else if (node.tagName().equals("sup", ignoreCase = true)) {
+                node.text().replaceScriptCharactersOrNull(SubSuperScripts.superscripts)?.let { node.text(it) }
             }
 
-            val href = node.absUrl("href")
-
-            //Try to resolve into an online link
-            for (type in DocSourceType.entries) {
-                val effectiveUrl = type.toEffectiveURL(href)
-                if (!doesStartByLocalhost(effectiveUrl)) { //If it's a valid link then don't remove it
-                    node.attr("href", effectiveUrl)
-                    return
-                }
-            }
-
-            //If no online URL has been found then do not link to localhost href(s)
-            node.removeAttr("href")
+            return
         }
 
-        override fun tail(node: Node, depth: Int) {}
-    })
+        val href = node.absUrl("href")
+
+        //Try to resolve into an online link
+        for (type in DocSourceType.entries) {
+            val effectiveUrl = type.toEffectiveURL(href)
+            if (!doesStartByLocalhost(effectiveUrl)) { //If it's a valid link then don't remove it
+                node.attr("href", effectiveUrl)
+                return
+            }
+        }
+
+        //If no online URL has been found then do not link to localhost href(s)
+        node.removeAttr("href")
+    }
+}
+
+internal fun JavadocElement.toMarkdown(): String {
+    targetElement.traverse(SubSuperScriptsVisitor)
 
     val html = targetElement.outerHtml()
 //			.replaceAll("<code>([^/]*?)<a href=\"(.*?)\">(\\X*?)</a>([^/]*?)</code>", "<code>$1</code><a href=\"$2\"><code>$1</code></a><code>right</code>");
